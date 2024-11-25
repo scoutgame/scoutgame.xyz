c4993eb84c0180500195263bc8711e8e0bb3be3d
mattcasey <mattwad@gmail.com>
2024-11-25 13:18:36 -0700

    update config

--- scripts/query.ts
+++ scripts/query.ts
@@ -1,9 +1,37 @@
 import { prisma } from '@charmverse/core/prisma-client';
 import { prettyPrint } from '@packages/utils/strings';
-
+import { sendPointsForMiscEvent } from '@packages/scoutgame/points/builderEvents/sendPointsForMiscEvent';
 async function query() {
-  const scout = await prisma.scout.findFirst({ where: { path: 'mattcasey' } });
+  const scout = await prisma.scout.findFirst({
+    where: { path: 'alexbita' }
+    // include: {
+    //   nftPurchaseEvents: {
+    //     select: {
+    //       pointsValue: true,
+    //       tokensPurchased: true,
+    //       builderNFT: {
+    //         select: {
+    //           builder: {
+    //             select: {
+    //               displayName: true,
+    //               path: true
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+  });
   prettyPrint(scout);
+  // await sendPointsForMiscEvent({
+  //   builderId: scout!.id,
+  //   points: 50,
+  //   claimed: true,
+  //   description: 'Refund for suspended builders: futreall and mdqst',
+  //   hideFromNotifications: true,
+  //   earnedAs: 'scout'
+  // });
 }
 
 query();
