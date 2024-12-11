a3839988296a44ef0bb957196b5bcfe90a0de145
Matt Casey <mattwad@gmail.com>
2024-12-11 15:07:29 -0700

    update Celo page (#12)

--- packages/scoutgame-ui/src/components/info/partner-rewards/CeloPage.tsx
+++ packages/scoutgame-ui/src/components/info/partner-rewards/CeloPage.tsx
@@ -56,8 +56,9 @@ function Document() {
         How it works
       </Typography>
       <Typography>
-        Qualified projects will mark their issues with a Tier in GitHub. The Tier determines the builder's reward for
-        merging a PR that addresses the issue. Unmarked issues will default to the Common Tier.
+        Ecosystem projects, that you can find in the list below (link list below), will mark their issues with a Tier in
+        GitHub. The Tier determines the builder's reward offered by Celo for merging a PR that addresses the issue.
+        Unmarked issues will default to the Common Tier.
       </Typography>
       <Table>
         <TableHead>
@@ -79,10 +80,39 @@ function Document() {
         How to contribute
       </Typography>
       <Typography>
+        Open Source contributions can be overwhelming when you are staring out. It is important to check a repository
+        for a contribution guide. If they don't offer one, you can refer back to a general one or use the{' '}
         <Link href='https://docs.celo.org/general/ecosystem/contributors' target='_blank' rel='noreferrer'>
-          Learn how to contribute to Celo
-        </Link>
+          Open Source Contribution guide
+        </Link>{' '}
+        provided by Celo
       </Typography>
+      <Typography>Here some general steps to get started if you are new to open source contributions:</Typography>
+      <List listStyleType='decimal'>
+        <ListItem>
+          Understand the repositories architecture and functions (e.g., if a repo is about EIP make sure you understand
+          the structure of EIPs)
+        </ListItem>
+        <ListItem>Check the reported issue and make sure you understand what is asked of you</ListItem>
+        <ListItem>
+          Create a fork of the repository and clone your fork to your local machine. You need to for the repository, as
+          you won't have direct write access to the main repository
+        </ListItem>
+        <ListItem>Run the code and reproduce the same behaviour</ListItem>
+        <ListItem>
+          Clarify all open questions from the issue. Make sure you dive deep into the repo before asking questions but
+          also don’t waste time getting lost. Most of the time, when formulating a question, everything will become
+          clear.
+        </ListItem>
+        <ListItem>If the coding language is unfamiliar to you, do a simple introduction course</ListItem>
+        <ListItem>
+          Maybe try test first approach, where you write the test first to make sure to check for the error that occurs
+        </ListItem>
+        <ListItem>Implement your solution</ListItem>
+        <ListItem>Write tests if you haven't done it yet</ListItem>
+        <ListItem>Write documentation (based on how documentation is done in that repository)</ListItem>
+        <ListItem>Create a PR mentioning the Issue and explaining the solution</ListItem>
+      </List>
       <Typography variant='h6' color='secondary' mt={2}>
         Qualified Celo Projects:
       </Typography>
@@ -90,6 +120,11 @@ function Document() {
         <ListItem>
           <Link href='https://github.com/celo-org/faucet' target='_blank' rel='noreferrer'>
             https://github.com/celo-org/faucet
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link href='https://github.com/mento-protocol/celo-composer' target='_blank' rel='noreferrer'>
+            https://github.com/mento-protocol/celo-composer
           </Link>
         </ListItem>
         <ListItem>
@@ -103,18 +138,8 @@ function Document() {
           </Link>
         </ListItem>
         <ListItem>
-          <Link href='https://github.com/mento-protocol/mento-web' target='_blank' rel='noreferrer'>
-            https://github.com/mento-protocol/mento-web
-          </Link>
-        </ListItem>
-        <ListItem>
           <Link href='https://github.com/mento-protocol/mento-sdk' target='_blank' rel='noreferrer'>
             https://github.com/mento-protocol/mento-sdk
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href='https://github.com/celo-org/celo-composer' target='_blank' rel='noreferrer'>
-            https://github.com/celo-org/celo-composer
           </Link>
         </ListItem>
         <ListItem>
