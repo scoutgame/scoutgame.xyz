71213e9a839e01c4ed83508ade2c55a96065fad3
Safwan Shaheer <devorein00@gmail.com>
2024-12-12 00:27:53 +0600

    merge scout records

--- apps/scoutgame/lib/users/mergeUserAccount.ts
+++ apps/scoutgame/lib/users/mergeUserAccount.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@charmverse/core/prisma-client';
+import { currentSeason } from '@packages/scoutgame/dates';
 
 export const mergeUserAccount = async ({
   userId,
@@ -62,5 +63,122 @@ export const mergeUserAccount = async ({
         deletedAt: new Date()
       }
     });
+
+    await tx.scoutMergeEvent.create({
+      data: {
+        mergedFromId: mergedUser.id,
+        mergedToId: userId,
+        mergedRecords: {
+          farcasterId,
+          telegramId
+        }
+      }
+    });
+
+    await tx.nFTPurchaseEvent.updateMany({
+      where: {
+        scoutId: mergedUser.id
+      },
+      data: {
+        scoutId: userId
+      }
+    });
+
+    await tx.pointsReceipt.updateMany({
+      where: {
+        recipientId: mergedUser.id
+      },
+      data: {
+        recipientId: userId
+      }
+    });
+
+    await tx.userWeeklyStats.deleteMany({
+      where: {
+        userId: mergedUser.id
+      }
+    });
+
+    const mergedUserSeasonStats = await tx.userSeasonStats.findFirstOrThrow({
+      where: {
+        userId: mergedUser.id,
+        season: currentSeason
+      }
+    });
+
+    await tx.userSeasonStats.update({
+      where: {
+        userId_season: {
+          userId,
+          season: currentSeason
+        }
+      },
+      data: {
+        pointsEarnedAsBuilder: {
+          increment: mergedUserSeasonStats.pointsEarnedAsBuilder
+        },
+        pointsEarnedAsScout: {
+          increment: mergedUserSeasonStats.pointsEarnedAsScout
+        },
+        nftOwners: {
+          increment: mergedUserSeasonStats.nftOwners ?? 0
+        },
+        nftsSold: {
+          increment: mergedUserSeasonStats.nftsSold ?? 0
+        },
+        nftsPurchased: {
+          increment: mergedUserSeasonStats.nftsPurchased
+        }
+      }
+    });
+
+    const mergedUserAllTimeStats = await tx.userAllTimeStats.findFirstOrThrow({
+      where: {
+        userId: mergedUser.id
+      }
+    });
+
+    await tx.userAllTimeStats.update({
+      where: {
+        userId
+      },
+      data: {
+        pointsEarnedAsBuilder: {
+          increment: mergedUserAllTimeStats.pointsEarnedAsBuilder
+        },
+        pointsEarnedAsScout: {
+          increment: mergedUserAllTimeStats.pointsEarnedAsScout
+        }
+      }
+    });
+
+    await tx.pendingNftTransaction.updateMany({
+      where: {
+        userId: mergedUser.id
+      },
+      data: {
+        userId
+      }
+    });
+
+    await tx.tokensReceipt.updateMany({
+      where: {
+        recipientId: mergedUser.id
+      },
+      data: {
+        recipientId: userId
+      }
+    });
+
+    await tx.referralCodeEvent.updateMany({
+      where: {
+        refereeId: mergedUser.id
+      },
+      data: {
+        refereeId: userId
+      }
+    });
+
+    // Skipping social claim and daily claim events, partner reward events and talent profile
   });
 };
