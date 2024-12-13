e6d7a726009910e1a97eb0216fcf2df72d5e423f
Safwan Shaheer <devorein00@gmail.com>
2024-12-13 18:58:28 +0600

    Refactored user session route using sg router package

--- apps/telegram/app/api/session/user/route.ts
+++ apps/telegram/app/api/session/user/route.ts
@@ -1,15 +1,1 @@
-import { getSession } from '@packages/scoutgame/session/getSession';
-import { getUserFromSession } from '@packages/scoutgame/session/getUserFromSession';
-import { NextResponse } from 'next/server';
-
-// This API Route is non-blocking and called on every page load. Use it to refresh things about the current user
-export async function GET() {
-  const user = await getUserFromSession({ sameSite: 'none' });
-  const session = await getSession();
-  if (user?.deletedAt) {
-    // Logout the user if they have been deleted
-    session.destroy();
-    return NextResponse.json(null);
-  }
-  return NextResponse.json(user);
-}
+export * from '@packages/scoutgame-router/api/session/user/route';
