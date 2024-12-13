c7a75a3597c891a3392f7a18a5c2a134709417b6
mattcasey <mattwad@gmail.com>
2024-12-13 16:47:51 -0700

    add expoort for moxie bonus

--- apps/admin/app/api/partners/moxie/route.ts
+++ apps/admin/app/api/partners/moxie/route.ts
@@ -1,119 +1,25 @@
 import { log } from '@charmverse/core/log';
 import { prisma } from '@charmverse/core/prisma-client';
 import { currentSeason, getLastWeek } from '@packages/scoutgame/dates';
-import { airstackRequest } from '@packages/scoutgame/moxie/airstackRequest';
-import { uniq } from 'lodash';
 import { v4 } from 'uuid';
 
-export const dynamic = 'force-dynamic';
+import { respondWithTSV } from 'lib/nextjs/respondWithTSV';
 
+import { getMoxieCandidates } from './export/route';
+
+export const dynamic = 'force-dynamic';
 export async function GET() {
   const lastWeek = getLastWeek();
-  const builders = await prisma.scout.findMany({
-    where: {
-      builderStatus: 'approved',
-      farcasterId: {
-        not: null
-      },
-      hasMoxieProfile: true,
-      userWeeklyStats: {
-        some: {
-          week: lastWeek,
-          gemsCollected: {
-            gt: 0
-          }
-        }
-      }
-    },
-    orderBy: {
-      farcasterId: 'asc'
-    },
-    select: {
-      id: true,
-      farcasterId: true,
-      path: true,
-      builderNfts: {
-        where: {
-          season: currentSeason
-        },
-        select: {
-          nftSoldEvents: {
-            select: {
-              scout: {
-                select: {
-                  farcasterId: true,
-                  id: true
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  });
+  const candidates = await getMoxieCandidates({ week: lastWeek, season: currentSeason });
 
-  if (builders.length === 0) {
-    log.info('No builders found', { lastWeek, currentSeason });
-    return new Response('No builders found', { status: 204 });
+  if (candidates.length === 0) {
+    log.info('No candidates found', { lastWeek, currentSeason });
+    return new Response('No candidates found', { status: 204 });
   }
 
-  const moxiePartnerRewardEvents = await prisma.partnerRewardEvent.findMany({
-    where: {
-      week: lastWeek,
-      partner: 'moxie',
-      season: currentSeason
-    },
-    orderBy: {
-      userId: 'asc'
-    },
-    select: {
-      user: {
-        select: {
-          farcasterId: true,
-          id: true
-        }
-      }
-    }
-  });
+  const candidatesNeedingPayment = candidates.filter((row) => !row['Moxie sent']);
 
-  const moxiePartnerRewardEventUserFids = moxiePartnerRewardEvents.map((e) => e.user.farcasterId);
-  const scoutMoxieAmounts: Record<
-    number,
-    {
-      fid: number;
-      userId: string;
-      amount: number;
-    }
-  > = {};
-
-  await Promise.all(
-    builders.map(async (builder) => {
-      const builderFid = builder.farcasterId as number;
-      const scoutsFids = builder.builderNfts
-        .map((nft) => nft.nftSoldEvents.map((e) => e.scout.farcasterId))
-        .flat()
-        .filter((scoutFid) => scoutFid && !moxiePartnerRewardEventUserFids.includes(scoutFid)) as number[];
-
-      for (const scoutFid of uniq(scoutsFids)) {
-        const fanTokenAmount = await getMoxieFanTokenAmount({
-          builderFid,
-          scoutFid
-        });
-        if (fanTokenAmount) {
-          if (!scoutMoxieAmounts[scoutFid]) {
-            scoutMoxieAmounts[scoutFid] = {
-              fid: scoutFid,
-              amount: 0,
-              userId: builder.id
-            };
-          }
-          scoutMoxieAmounts[scoutFid].amount += 1;
-        }
-      }
-    })
-  );
-
-  if (Object.values(scoutMoxieAmounts).length === 0) {
+  if (candidatesNeedingPayment.length === 0) {
     log.info('No moxie amounts found', { lastWeek, currentSeason });
     return new Response('No moxie amounts found', { status: 204 });
   }
@@ -128,17 +34,17 @@ export async function GET() {
       body: JSON.stringify({
         id: v4(),
         timestamp: new Date().toISOString(),
-        data: Object.values(scoutMoxieAmounts).map(({ fid, amount }) => ({
+        data: candidatesNeedingPayment.map(({ 'Scout FID': fid, 'Moxie tokens earned': amount }) => ({
           fid,
-          amount: amount * 2000
+          amount
         }))
       })
     });
 
     await prisma.partnerRewardEvent.createMany({
-      data: Object.values(scoutMoxieAmounts).map(({ userId, amount }) => ({
+      data: candidatesNeedingPayment.map(({ 'Scout ID': userId, 'Moxie tokens earned': amount }) => ({
         reward: {
-          amount: amount * 2000
+          amount
         },
         week: lastWeek,
         partner: 'moxie',
@@ -150,39 +56,5 @@ export async function GET() {
     log.error('Error posting to moxie', { error: e });
   }
 
-  return new Response('Success', { status: 200 });
-}
-
-export async function getMoxieFanTokenAmount({
-  builderFid,
-  scoutFid
-}: {
-  builderFid: number;
-  scoutFid: number;
-}): Promise<number> {
-  const query = `
-    query GetPortfolioInfo {
-      MoxieUserPortfolios(
-        input: {
-          filter: {
-            fid: {_eq: "${scoutFid}"},
-            fanTokenSymbol: {
-              # Fan Token to check, symbol will be based on types:
-              # - User: fid:<FID>
-              # - Channel: cid:<CHANNEL-ID>
-              # - Network: id:farcaster
-              _eq: "fid:${builderFid}"
-            }
-          }
-        }
-      ) {
-        MoxieUserPortfolio {
-          amount: totalUnlockedAmount
-        }
-      }
-    }
-  `;
-  const data = await airstackRequest(query);
-  // console.log('data', data);
-  return data.data.MoxieUserPortfolios.MoxieUserPortfolio?.[0]?.amount || 0;
+  return respondWithTSV(candidates, `moxie-bonus_${lastWeek}.tsv`);
 }
