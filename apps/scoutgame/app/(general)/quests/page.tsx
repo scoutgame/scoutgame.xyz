fb3af75f8516360ca9bcdb8dd1a32e51dbc1539a
Valentin L <ludu.valentin@gmail.com>
2024-12-13 16:29:36 +0200

    Improve quests performence

--- apps/scoutgame/app/(general)/quests/page.tsx
+++ apps/scoutgame/app/(general)/quests/page.tsx
@@ -1,18 +1,26 @@
 import { getDailyClaims } from '@packages/scoutgame/claims/getDailyClaims';
 import { getQuests } from '@packages/scoutgame/quests/getQuests';
-import { getUserFromSession } from '@packages/scoutgame/session/getUserFromSession';
+import { getSession } from '@packages/scoutgame/session/getSession';
 import { getFriends } from '@packages/scoutgame/users/getFriends';
 import { safeAwaitSSRData } from '@packages/scoutgame/utils/async';
 import { QuestsPage } from '@packages/scoutgame-ui/components/quests/QuestsPage';
 
 export default async function Quests() {
-  const user = await getUserFromSession();
-  if (!user) {
+  const session = await getSession();
+  const scoutId = session.scoutId;
+
+  if (!scoutId) {
     return null;
   }
-  const [, friends = []] = await safeAwaitSSRData(getFriends(user.id));
-  const [, dailyClaims = []] = await safeAwaitSSRData(getDailyClaims(user.id));
-  const [, quests = []] = await safeAwaitSSRData(getQuests(user.id));
+
+  const allPromises = [getFriends(scoutId), getDailyClaims(scoutId), getQuests(scoutId)] as const;
+  const [error, data] = await safeAwaitSSRData(Promise.all(allPromises));
+
+  if (error) {
+    return null;
+  }
+
+  const [friends, dailyClaims, quests] = data;
 
   return <QuestsPage dailyClaims={dailyClaims} quests={quests} friends={friends} />;
 }
