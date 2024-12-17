4ec3402ea78b8e60ee7f51675e1bde4130e46067
mattcasey <mattwad@gmail.com>
2024-12-17 11:46:04 -0700

    .

--- packages/loops/src/registerLoopsContact.ts
+++ packages/loops/src/registerLoopsContact.ts
@@ -1,20 +1,30 @@
+import { log } from '@charmverse/core/log';
 import type { Scout } from '@charmverse/core/prisma';
 
 import type { LoopsUser } from './client';
 import { isEnabled, createOrUpdateContact } from './client';
+import { sendSignupEvent } from './sendSignupEvent';
 
 type ScoutFields = Pick<Scout, 'createdAt' | 'email' | 'displayName' | 'sendMarketing'>;
 
 // Creates a user if one does not exist
 // Call this whenever a user toggles subscriptions, ie. "emailNewsletter", or update their email
-export async function registerLoopsContact(user: ScoutFields, source: string) {
+export async function registerLoopsContact(scout: ScoutFields, source: string) {
   if (!isEnabled) {
     return { success: false, isNewContact: false };
   }
-  return createOrUpdateContact({
-    ...getLoopsUser(user),
-    source
-  });
+  try {
+    const result = await createOrUpdateContact({
+      ...getLoopsUser(scout),
+      source
+    });
+    if (result.isNewContact) {
+      await sendSignupEvent({ email: scout.email! });
+    }
+  } catch (error) {
+    log.error('Error registering loops contact', { error, scout });
+    return { success: false, isNewContact: false };
+  }
 }
 
 function getLoopsUser(scout: ScoutFields): Pick<LoopsUser, 'email' | 'createdAt' | 'firstName' | 'subscribed'> {
