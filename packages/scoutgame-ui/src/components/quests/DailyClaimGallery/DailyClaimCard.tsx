3c6ab5e028ab38dfa139b4db63ee920367ac028d
Valentin L <ludu.valentin@gmail.com>
2024-12-24 14:35:31 +0200

    Add animations to quest button

--- packages/scoutgame-ui/src/components/quests/DailyClaimGallery/DailyClaimCard.tsx
+++ packages/scoutgame-ui/src/components/quests/DailyClaimGallery/DailyClaimCard.tsx
@@ -9,15 +9,112 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { DateTime } from 'luxon';
 import Image from 'next/image';
 import { useAction } from 'next-safe-action/hooks';
+import { useMemo } from 'react';
 
 import { useUser } from '../../../providers/UserProvider';
 import { DailyClaimGift } from '../../claim/components/common/DailyClaimGift';
-import { BoxMotion } from '../../common/Motions/BoxMotion';
 
 const WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
+function AnimatedContent({ isClaimed, points }: { isClaimed: boolean; points: number }) {
+  return (
+    <AnimatePresence mode='wait' initial={false}>
+      {isClaimed ? (
+        <Stack
+          key='claimed'
+          direction='row'
+          gap={0.5}
+          alignItems='center'
+          zIndex={1}
+          position='absolute'
+          top={18}
+          bottom={0}
+          component={motion.div}
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 1 }}
+          transition={{ duration: 0.3, delay: 0.5 }}
+        >
+          <Typography fontWeight={600}>{points}</Typography>
+          <Image src='/images/profile/scout-game-profile-icon.png' alt='Scout game icon' width={15} height={8.5} />
+        </Stack>
+      ) : (
+        <Stack
+          key='unclaimed'
+          component={motion.div}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.3 }}
+          zIndex={1}
+          top={30}
+          bottom={0}
+          position='absolute'
+        >
+          <Image src='/images/quests/question-icon.png' alt='Quest icon' width={24} height={24} />
+        </Stack>
+      )}
+    </AnimatePresence>
+  );
+}
+
+export function AnimatedGift({
+  isClaimed,
+  isBonus = false,
+  variant
+}: {
+  isClaimed: boolean;
+  isBonus?: boolean;
+  variant: 'disabled' | 'secondary' | 'primary';
+}) {
+  return (
+    <AnimatePresence initial={false}>
+      {!isClaimed && (
+        <Stack sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+          {isBonus ? (
+            <Stack direction='row' gap={0.5} alignItems='flex-end'>
+              <DailyClaimGift variant={variant} size={44} />
+              <DailyClaimGift variant={variant} size={70} />
+              <DailyClaimGift variant={variant} size={44} />
+            </Stack>
+          ) : (
+            <DailyClaimGift variant={variant} size={64} />
+          )}
+        </Stack>
+      )}
+    </AnimatePresence>
+  );
+}
+
+export function AnimatedClaimedIcon({ isClaimed }: { isClaimed: boolean }) {
+  return (
+    <AnimatePresence>
+      {isClaimed && (
+        <Stack
+          component={motion.div}
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+        >
+          <CheckCircleIcon
+            fontSize='small'
+            color='secondary'
+            data-test='claimed-icon'
+            sx={{
+              position: 'absolute',
+              top: 5,
+              right: 5
+            }}
+          />
+        </Stack>
+      )}
+    </AnimatePresence>
+  );
+}
+
 export function DailyClaimCard({
-  dailyClaim,
+  dailyClaim: _dailyClaim,
   hasClaimedStreak
 }: {
   dailyClaim: DailyClaim;
@@ -33,6 +130,12 @@ export function DailyClaimCard({
       refreshUser();
     }
   });
+
+  const dailyClaim = useMemo(
+    () => (result?.data?.points ? { ..._dailyClaim, claimed: true, points: result?.data?.points } : _dailyClaim),
+    [result?.data?.points]
+  );
+
   const currentWeekDay = DateTime.fromJSDate(new Date()).weekday;
   const isPastDay = currentWeekDay > dailyClaim.day;
   const isClaimToday = currentWeekDay === dailyClaim.day;
@@ -64,7 +167,8 @@ export function DailyClaimCard({
 
   return (
     <Stack
-      component={canClaim ? BoxMotion : Stack}
+      component={motion.div}
+      whileTap={{ scale: canClaim ? 0.9 : 1 }}
       sx={{
         backgroundColor: isClaimed
           ? 'background.light'
@@ -89,83 +193,30 @@ export function DailyClaimCard({
     >
       <Stack
         component={motion.div}
-        whileHover={{ scale: 1, rotate: 0, transition: { duration: 0.3, ease: 'easeOut' } }} // Simplified to just stay at normal scale on hover
-        animate={
-          canClaim
-            ? {
-                scale: [1, 1.1, 1, 1.1, 1],
-                rotate: [0, -15, 0, 15, 0],
-                transition: {
-                  duration: 2,
-                  ease: 'easeInOut',
-                  times: [0, 0.2, 0.4, 0.6, 0.8, 1],
-                  repeat: Infinity
-                }
-              }
-            : undefined
-        }
+        whileHover={{ scale: 1, rotate: 0, transition: { duration: 0.3, ease: 'easeOut' } }}
+        variants={{
+          claim: {
+            scale: [1, 1.1, 1, 1.1, 1],
+            rotate: [0, -15, 0, 15, 0],
+            transition: {
+              duration: 2,
+              ease: 'easeInOut',
+              times: [0, 0.2, 0.4, 0.6, 0.8, 1],
+              repeat: Infinity
+            }
+          },
+          default: { scale: 1, rotate: 0 }
+        }}
+        animate={canClaim ? 'claim' : 'default'}
         flex={1}
         position='relative'
         alignItems='center'
         justifyContent='center'
         width='100%'
       >
-        {!isClaimed ? (
-          <Stack sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-            {dailyClaim.isBonus ? (
-              <Stack direction='row' gap={0.5} alignItems='flex-end'>
-                <DailyClaimGift variant={variant} size={44} />
-                <DailyClaimGift variant={variant} size={70} />
-                <DailyClaimGift variant={variant} size={44} />
-              </Stack>
-            ) : (
-              <DailyClaimGift variant={variant} size={64} />
-            )}
-          </Stack>
-        ) : (
-          <CheckCircleIcon
-            fontSize='small'
-            color='secondary'
-            data-test='claimed-icon'
-            sx={{
-              position: 'absolute',
-              top: 5,
-              right: 5
-            }}
-          />
-        )}
-        <AnimatePresence initial={false}>
-          {isClaimed ? (
-            <Stack
-              direction='row'
-              gap={0.5}
-              alignItems='center'
-              zIndex={1}
-              top={7.5}
-              position='relative'
-              component={motion.div}
-              initial={{ scale: 0, opacity: 0 }}
-              animate={{ scale: 1.1, opacity: 1 }}
-              transition={{ duration: 1, ease: 'easeIn' }}
-            >
-              <Typography fontWeight={600}>{dailyClaim.points}</Typography>
-              <Image src='/images/profile/scout-game-profile-icon.png' alt='Scout game icon' width={15} height={8.5} />
-            </Stack>
-          ) : (
-            <Stack
-              component={motion.div}
-              initial={{ opacity: 1, scale: 1 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ scale: [1.1, 0], opacity: 0 }}
-              transition={{ duration: 1, ease: 'easeOut' }}
-              zIndex={1}
-              top={7.5}
-              position='relative'
-            >
-              <Image src='/images/quests/question-icon.png' alt='Quest icon' width={24} height={24} />
-            </Stack>
-          )}
-        </AnimatePresence>
+        <AnimatedGift isClaimed={isClaimed} isBonus={dailyClaim.isBonus} variant={variant} />
+        <AnimatedClaimedIcon isClaimed={isClaimed} />
+        <AnimatedContent isClaimed={isClaimed} points={dailyClaim.points} />
       </Stack>
       <Typography
         variant='body2'
