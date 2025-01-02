a325ead49a3632721fe9aaf3d88a7d427811e003
motechFR <mo@anyahq.co>
2025-01-02 22:11:49 +0200

    nft season

--- packages/scoutgame/src/builderNfts/mintNFT.ts
+++ packages/scoutgame/src/builderNfts/mintNFT.ts
@@ -2,6 +2,9 @@ import type { BuilderNftType } from '@charmverse/core/prisma-client';
 import { prisma } from '@charmverse/core/prisma-client';
 import type { Address } from 'viem';
 
+import type { ISOWeek } from '../dates/config';
+import { getCurrentSeasonStart } from '../dates/utils';
+
 import { getBuilderContractMinterClient } from './clients/builderContractMinterWriteClient';
 import { getBuilderContractStarterPackMinterClient } from './clients/builderContractStarterPackMinterWriteClient';
 import { recordNftMint } from './recordNftMint';
@@ -14,13 +17,17 @@ export type MintNFTParams = {
   paidWithPoints: boolean; // whether to subtract from the scout's points
   scoutId: string;
   nftType: BuilderNftType;
+  season?: ISOWeek;
 };
 
 export async function mintNFT(params: MintNFTParams) {
+  const season = params.season ?? getCurrentSeasonStart();
+
   const { builderNftId, recipientAddress, amount, scoutId, nftType } = params;
   const builderNft = await prisma.builderNft.findFirstOrThrow({
     where: {
-      id: builderNftId
+      id: builderNftId,
+      season
     }
   });
   const apiClient =
