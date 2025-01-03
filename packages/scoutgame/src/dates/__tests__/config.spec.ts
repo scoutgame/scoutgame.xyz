763b380ae2875dfe62d19505c09a0885d5ab9a25
motechFR <mo@anyahq.co>
2025-01-03 15:48:56 +0200

    Restore the correct preseason 02

--- packages/scoutgame/src/dates/__tests__/config.spec.ts
+++ packages/scoutgame/src/dates/__tests__/config.spec.ts
@@ -4,10 +4,10 @@ import { getCurrentSeasonStart } from '../utils';
 
 describe('season dates', () => {
   it('should return the correct season iso-week date', () => {
-    log.info(
-      `This test is failing because we have modified the season list. Restore pre season 02 to start at 2025-W02 for this test to pass`
-    );
     const season = getCurrentSeasonStart('2025-W01');
     expect(season).toBe('2024-W41');
+
+    const season2 = getCurrentSeasonStart('2025-W02');
+    expect(season2).toBe('2025-W02');
   });
 });
