8e57c99cd56c817b845047537c17c420f897a830
motechFR <mo@anyahq.co>
2025-01-08 13:24:47 +0100

    Add test coverage for deterministic uuid and add jest config

---
+++ packages/utils/jest.config.ts
@@ -0,0 +1,7 @@
+export default {
+  // Teardown function after all tests run
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest'
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx']
+};
--- packages/utils/src/__tests__/strings.spec.ts
+++ packages/utils/src/__tests__/strings.spec.ts
@@ -1,4 +1,5 @@
 import { concatenateStringValues } from '../strings';
+import { uuidFromNumber } from '../uuid';
 
 describe('concatenateStringValues', () => {
   it('should concatenate all string values from the object into an array', () => {
@@ -66,5 +67,25 @@ describe('concatenateStringValues', () => {
 
     const result = concatenateStringValues(input);
     expect(result).toEqual(['test', 'example']);
+  });
+});
+
+describe('uuidFromNumber', () => {
+  it('should generate a deterministic UUID v4 based on an integer input', () => {
+    const input = 1234567890;
+    const result = uuidFromNumber(input);
+    const firstUuid = 'c775e7b7-57ed-4630-8d0a-a1113bd10266';
+    expect(result).toEqual(firstUuid);
+
+    const result2 = uuidFromNumber(input);
+    expect(result2).toEqual(firstUuid);
+
+    const otherInput = 1234567891;
+    const result3 = uuidFromNumber(otherInput);
+    const secondUuid = '523aa18e-cb89-4c51-bbdb-e28c57e10a92';
+    expect(result3).toEqual(secondUuid);
+
+    const result4 = uuidFromNumber(otherInput);
+    expect(result4).toEqual(secondUuid);
   });
 });
