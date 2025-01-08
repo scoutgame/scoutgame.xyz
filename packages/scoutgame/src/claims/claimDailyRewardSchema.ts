1e501fb8a3f50a2df65ff5cea2416350ad4423b6
Valentin L <ludu.valentin@gmail.com>
2025-01-08 19:40:14 +0200

    Validate date to be today

--- packages/scoutgame/src/claims/claimDailyRewardSchema.ts
+++ packages/scoutgame/src/claims/claimDailyRewardSchema.ts
@@ -1,6 +1,13 @@
 import * as yup from 'yup';
 
+import { getServerDate } from '../utils/getServerDate';
+
 export const claimDailyRewardSchema = yup.object({
   isBonus: yup.boolean(),
-  dayOfWeek: yup.number().required()
+  dayOfWeek: yup
+    .number()
+    .required()
+    .min(1)
+    .max(7)
+    .test('valid-today', 'Invalid day.', (value) => getServerDate().weekday === value)
 });
