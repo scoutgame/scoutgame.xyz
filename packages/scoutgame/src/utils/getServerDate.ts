2dffa1db37018856a8b79ca6c0aa81f020de279a
Valentin L <ludu.valentin@gmail.com>
2025-01-08 18:16:21 +0200

    Update to UTC

--- packages/scoutgame/src/utils/getServerDate.ts
+++ packages/scoutgame/src/utils/getServerDate.ts
@@ -1,8 +1,5 @@
 import { DateTime } from 'luxon';
 
 export function getServerDate(): DateTime {
-  const TIMEZONE = 'America/New_York';
-  const LOCALE = 'en-US';
-  const date = DateTime.now().plus({ days: 2 }).setZone(TIMEZONE).setLocale(LOCALE);
-  return date;
+  return DateTime.utc();
 }
