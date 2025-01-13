5d7996ad82c93940b3ba1c7ce616a43e423ed4ab
Valentin L <ludu.valentin@gmail.com>
2025-01-13 15:35:23 +0200

    make sure we don't show an empty list

--- packages/scoutgame-ui/src/components/quests/Connector/ConnectorContainer.tsx
+++ packages/scoutgame-ui/src/components/quests/Connector/ConnectorContainer.tsx
@@ -5,5 +5,9 @@ import { Connector } from './Connector';
 export async function ConectorContainer() {
   const data = await getTop5ConnectorsToday();
 
+  if (data.length === 0) {
+    return null;
+  }
+
   return <Connector topUsers={data} />;
 }
