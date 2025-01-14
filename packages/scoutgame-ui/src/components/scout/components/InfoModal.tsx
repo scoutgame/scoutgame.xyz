2adb4a13f2f4c651c257f1d6091a4b8d48f1dbcd
Safwan Shaheer <devorein00@gmail.com>
2025-01-14 21:37:34 +0600

    Fixed builder card activity tooltip content

--- packages/scoutgame-ui/src/components/scout/components/InfoModal.tsx
+++ packages/scoutgame-ui/src/components/scout/components/InfoModal.tsx
@@ -5,44 +5,8 @@ import type { SxProps } from '@mui/material';
 import { IconButton, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
 
+import { BuilderCardActivityTooltip } from '../../common/Card/BuilderCard/BuilderCardActivity/BuilderCardActivityTooltip';
 import { Dialog } from '../../common/Dialog';
-
-function BuilderInfoContent() {
-  return (
-    <Stack>
-      <Typography variant='body2' fontWeight='bold'>
-        RANK
-      </Typography>
-      <Typography mb={2}>
-        Builder’s current rank in the weekly Gem competition. The Builder’s rank at the end of the week determines the
-        amount of Scout Points earned. Higher rank = more Scout Points.
-      </Typography>
-
-      <Typography variant='body2' fontWeight='bold'>
-        PRICE
-      </Typography>
-      <Typography mb={2}>
-        The current cost of the Builder’s Card in Scout Points. Purchase the Builder’s Card to scout the Builder and
-        earn scout points as they move up the leader board. Builder cards may also be purchased with USDC or ETH on
-        Optimism, Base, Arbitrum, and Zora.
-      </Typography>
-
-      <Typography variant='body2' fontWeight='bold'>
-        POINTS
-      </Typography>
-      <Typography mb={2}>Scout Points earned by the Builder this season to date.</Typography>
-
-      <Typography variant='body2' fontWeight='bold'>
-        CARDS
-      </Typography>
-      <Typography mb={2}>
-        Total number of Builder Cards sold for that Builder. The price of the Builder’s card increases by 20 Scout
-        Points after each sale. At the end of the week, Scouts that hold a Builder’s card(s) receive a share of that
-        Builder’s Scout Point earnings.
-      </Typography>
-    </Stack>
-  );
-}
 
 function ScoutInfoContent() {
   return (
@@ -85,7 +49,7 @@ export function InfoModal({ builder = false, sx }: { builder?: boolean; sx?: SxP
         <Typography my={2} variant='h5' color='secondary'>
           {builder ? 'Builders' : 'Scouts'} Data Table
         </Typography>
-        {builder ? <BuilderInfoContent /> : <ScoutInfoContent />}
+        {builder ? <BuilderCardActivityTooltip /> : <ScoutInfoContent />}
       </Dialog>
     </>
   );
