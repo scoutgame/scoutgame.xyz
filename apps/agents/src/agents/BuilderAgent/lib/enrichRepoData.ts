17a0327321259fec13a8c847273aec655c0eab3a
motechFR <mo@anyahq.co>
2025-01-24 00:46:29 +0100

    Cleanup the script

--- apps/agents/src/agents/BuilderAgent/lib/enrichRepoData.ts
+++ apps/agents/src/agents/BuilderAgent/lib/enrichRepoData.ts
@@ -367,11 +367,11 @@ Expected output format is JSON object with the following fields:
 
 // Fix the github file filtering to ignore config, aws and other files that dont provide value
 
-processRepo({ repoOwner: 'scoutgame', repoName: 'scoutgame.xyz' })
-  .then((result) => {
-    log.info('result', result);
-  })
-  .catch((error) => {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  });
+// processRepo({ repoOwner: 'scoutgame', repoName: 'scoutgame.xyz' })
+//   .then((result) => {
+//     log.info('result', result);
+//   })
+//   .catch((error) => {
+//     // eslint-disable-next-line no-console
+//     console.error(error);
+//   });
