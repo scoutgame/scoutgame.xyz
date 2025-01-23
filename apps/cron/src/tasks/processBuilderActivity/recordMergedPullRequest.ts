d283f10f42bf93609d6b418eb107f42925ee3cb2
Matt Casey <mattwad@gmail.com>
2025-01-23 12:48:24 -0700

    Revert "Allow merging multiple pull request per repo per day (#213)"
    This reverts commit 61a022db3a589fb1f3d51ea81ac04aba868438fe.

--- apps/cron/src/tasks/processBuilderActivity/recordMergedPullRequest.ts
+++ apps/cron/src/tasks/processBuilderActivity/recordMergedPullRequest.ts
@@ -129,6 +129,13 @@ export async function recordMergedPullRequest({
     }
   }
 
+  const existingGithubEventToday = previousGitEvents.some((event) => {
+    if (event.repoId !== pullRequest.repository.id) {
+      return false;
+    }
+    return isToday(event.createdAt, DateTime.fromISO(pullRequest.createdAt, { zone: 'utc' }));
+  });
+
   return prisma.$transaction(async (tx) => {
     const githubUser = await tx.githubUser.upsert({
       where: {
@@ -155,7 +162,7 @@ export async function recordMergedPullRequest({
         completedAt: pullRequest.mergedAt
       }
     });
-    if (githubUser.builderId) {
+    if (githubUser.builderId && !existingGithubEventToday) {
       const builder = await tx.scout.findUniqueOrThrow({
         where: {
           id: githubUser.builderId
