02a808447b49da36a63b8cf47afd391790f7bb50
motechFR <mo@anyahq.co>
2025-01-24 13:10:06 +0100

    Scout AI agent for builders (#163)
    * WIP
    
    * add a script to generate repos for chatbot
    
    * Add package generator script
    
    * Setup the Koa app with Telegram IO
    
    * Cleanup scripts
    
    * Setup LLM capabilities
    
    * Setup the base agent
    
    * Setup CI
    
    * Hook the agent up to telegram
    
    * Finish hooking up ScoutGame and add the CTA to the mini app
    
    * Add telegram to packages
    
    * Integrate the whole stack
    
    * Cleanup
    
    * Fix ecr repository
    
    * Cleanup
    
    * Fix Docker Compose file
    
    * Fix prod build
    
    * Make sure JSON is copied over
    
    * Add uptime to the API response
    
    * Cleanup search
    
    * Fix telegram endpoint security
    
    * Cleanup security mechanism
    
    * Simplify prompt and fix search
    
    * Cleanup
    
    * Cleanup
    
    * Bump core
    
    * Address all feedback
    
    * Rename to agents folder
    
    * Fix renaming
    
    * Cleanup type imports
    
    * Cleanup test command to pass with no tests
    
    * Add logging for the webhook url
    
    * Setup text ingestion script
    
    * Add file tree parser
    
    * Setup project analysis
    
    * Cleanup parser
    
    * Cleanup the script
    
    * Cleanup the script
    
    * Extra cleanup
    
    * Add enriched pipeline
    
    * Better logging
    
    * Cleanup the pipeline again
    
    * Fix randomisation
    
    * Add parsed repo descriptions
    
    * Add missing repos
    
    ---------
    
    Co-authored-by: mattcasey <mattwad@gmail.com>

---
+++ packages/llm/src/requestChatCompletion.ts
@@ -0,0 +1,96 @@
+import { InvalidInputError } from '@charmverse/core/errors';
+import OpenAI from 'openai';
+
+import type { LLMModel } from './constants';
+import { LLMModelsList } from './constants';
+import type { ChatCompletionResponse, ChatMessage, ToolDefinition } from './interfaces';
+import { llmLogger } from './logger';
+
+/**
+ * @model - Defaults to gpt-4o-mini
+ */
+type ChatCompletionRequest = {
+  prompt: string;
+  openAiApiKey: string;
+  systemPrompt?: string;
+  history?: ChatMessage[];
+  model?: LLMModel;
+  tools?: ToolDefinition[];
+};
+
+export async function requestChatCompletion({
+  prompt,
+  model = 'openaiGpt4oMini',
+  openAiApiKey,
+  systemPrompt,
+  history,
+  tools
+}: ChatCompletionRequest): Promise<ChatCompletionResponse> {
+  if (!openAiApiKey) {
+    throw new InvalidInputError('OpenAI API key is required');
+  }
+
+  const input: ChatMessage[] = [];
+
+  if (systemPrompt) {
+    input.push({ role: 'system', content: systemPrompt });
+  }
+
+  if (history) {
+    input.push(...history);
+  }
+
+  input.push({ role: 'user', content: prompt });
+
+  const client = new OpenAI({
+    apiKey: openAiApiKey
+  });
+
+  const response = await client.chat.completions.create({
+    messages: input,
+    model: LLMModelsList[model],
+    tools: tools?.map((tool) => ({
+      type: 'function',
+      function: {
+        name: tool.name,
+        description: tool.description,
+        parameters: {
+          type: 'object',
+          properties: tool.parameters,
+          required: tool.required
+        }
+      }
+    }))
+  });
+
+  function parseToolCallArguments(argsString: string) {
+    try {
+      return JSON.parse(argsString);
+    } catch (e) {
+      llmLogger.warn('Failed to parse tool call arguments:', e);
+      return argsString;
+    }
+  }
+
+  if (response.choices[0].message.tool_calls) {
+    // // llmLogger.info(
+    //   'Agent requested a tool call',
+    //   JSON.stringify(
+    //     {
+    //       toolCalls: response.choices[0].message.tool_calls
+    //     },
+    //     null,
+    //     2
+    //   )
+    // );
+    (response as ChatCompletionResponse).tool_calls = response.choices[0].message.tool_calls?.map((toolCall) => ({
+      name: toolCall.function.name,
+      arguments: parseToolCallArguments(toolCall.function.arguments)
+    }));
+  }
+
+  return {
+    ...response,
+    message: response.choices[0].message.content
+  } as ChatCompletionResponse;
+}
