b7ee99c31ee918e5403eda0493de521b13a129e2
Valentin <ludu.valentin@gmail.com>
2025-02-04 12:53:30 +0200

    Improve builder onboarding (#261)
    * Always redirect builders from spam to builders page
    
    * refresh user before if needed
    
    * Modify UI
    
    * Make invite modal smaller
    
    * Improve invite modal
    
    * Ensure the callback returns the builder flow

--- packages/scoutgame-ui/src/components/common/SinglePageWrapper.tsx
+++ packages/scoutgame-ui/src/components/common/SinglePageWrapper.tsx
@@ -1,17 +1,26 @@
 import type { BoxProps } from '@mui/material/Box';
 import Box from '@mui/material/Box';
 
-export function SinglePageWrapper({ children, ...props }: BoxProps) {
+export function SinglePageWrapper({ children, hasBorder, ...props }: BoxProps & { hasBorder?: boolean }) {
+  const border = hasBorder
+    ? {
+        sx: {
+          borderWidth: { xs: 0, sm: 1 },
+          borderStyle: { xs: 'none', sm: 'solid' },
+          borderColor: { xs: 'transparent', sm: 'secondary.main' }
+        }
+      }
+    : undefined;
   return (
     <Box
       position='relative'
       borderRadius={{ xs: 0, md: 2 }}
-      border={{ xs: 'none', sm: '1px solid var(--charm-palette-divider)' }}
       maxWidth='500px'
       height='100%'
       textAlign='left'
       mx='auto'
       p={{ xs: 2, md: 3 }}
+      {...border}
       {...props}
     >
       {children}
