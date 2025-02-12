149c62258cbe63a391b9692f2c27dc43d6c0bee7
motechFR <mo@anyahq.co>
2025-02-12 12:30:57 +0100

    Fix broken tests

--- packages/scoutgame/src/builderNfts/__tests__/refreshEstimatedPayouts.spec.ts
+++ packages/scoutgame/src/builderNfts/__tests__/refreshEstimatedPayouts.spec.ts
@@ -11,6 +11,11 @@ jest.unstable_mockModule('@packages/dates/utils', () => ({
   getCurrentSeasonStart: jest.fn((week) => week)
 }));
 
+// Mock this so we don't get an error in the dependency tree
+jest.unstable_mockModule('@packages/scoutgame/builders/getBuildersLeaderboardFromEAS', () => ({
+  getBuildersLeaderboardFromEAS: jest.fn()
+}));
+
 describe('refreshEstimatedPayouts', () => {
   it('should refresh the estimated payouts for a season, and zero out the payouts for builders who dont rank', async () => {
     const { refreshEstimatedPayouts } = await import('../refreshEstimatedPayouts');
