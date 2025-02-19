c8ec7de6e820cf27ec5184020571946057497075
mattcasey <mattwad@gmail.com>
2025-02-19 10:20:34 -0700

    add migration

---
+++ scripts/migrations/2024_02_19.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@charmverse/core/prisma-client';
+import { prettyPrint } from '@packages/utils/strings';
+import { getPublicClient } from '@packages/blockchain/getPublicClient';
+import { getBlockByDate } from '@packages/blockchain/getBlockByDate';
+// console.log('current week', getCurrentWeek());
+
+async function query() {
+  console.log(
+    await prisma.githubRepo.update({
+      where: { owner: 'gitcoinco', bonusPartner: 'octant' },
+      data: {
+        bonusPartner: null
+      }
+    })
+  );
+  console.log(
+    await prisma.githubRepo.update({
+      where: { owner: 'AbundanceProtocol', bonusPartner: 'octant' },
+      data: {
+        bonusPartner: null
+      }
+    })
+  );
+  console.log(
+    await prisma.githubRepo.update({
+      where: { owner: 'opensource-observer', bonusPartner: 'octant' },
+      data: {
+        bonusPartner: null
+      }
+    })
+  );
+}
+
+query();
