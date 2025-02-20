6eaf28661f92bdd9e03c0e323664726a96c2771e
Safwan Shaheer <devorein00@gmail.com>
2025-02-20 21:45:49 +0600

    removed hardcoded octant rewards when importing repos (#324)

--- packages/scoutgame/src/importReposByUser.ts
+++ packages/scoutgame/src/importReposByUser.ts
@@ -42,36 +42,12 @@ export async function importReposByUser(githubLogin: string) {
         defaultBranch: repo.default_branch,
         name: repo.name,
         ownerType: isOrg ? 'org' : 'user',
-        fork: repo.fork,
-        bonusPartner: 'octant'
+        fork: repo.fork
       }))
     });
     log.info(
       `Imported new repos from ${githubLogin}:`,
       notSaved.map((r) => r.name)
     );
-  }
-  if (reposInDB.length > 0) {
-    const alreadyHaveBonus = reposInDB.filter((r) => !!r.bonusPartner);
-    log.info(
-      `Updated existing repos for ${githubLogin}`,
-      await prisma.githubRepo.updateMany({
-        where: {
-          id: {
-            in: reposInDB.map((r) => r.id)
-          },
-          bonusPartner: null
-        },
-        data: {
-          bonusPartner: 'octant'
-        }
-      })
-    );
-    if (alreadyHaveBonus.length > 0) {
-      log.info(
-        'Some PRs already have bonus partner:',
-        alreadyHaveBonus.map((r) => `${r.owner}/${r.name} - ${r.bonusPartner}`)
-      );
-    }
   }
 }
