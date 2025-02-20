eb0c60e0ef7dd533be2f77ada1b315909ba3a034
Safwan Shaheer <devorein00@gmail.com>
2025-02-21 03:53:14 +0600

    auto login user using farcaster wallet

--- apps/scoutgame/components/common/ClientGlobals.tsx
+++ apps/scoutgame/components/common/ClientGlobals.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import { log } from '@charmverse/core/log';
 import sdk from '@farcaster/frame-sdk';
 import { getPlatform } from '@packages/mixpanel/platform';
+import { loginWithWalletAction } from '@packages/scoutgame/session/loginWithWalletAction';
 import { useDatadogLogger } from '@packages/scoutgame-ui/hooks/useDatadogLogger';
 import { useInitTelegramData } from '@packages/scoutgame-ui/hooks/useInitTelegramData';
+import { useLoginSuccessHandler } from '@packages/scoutgame-ui/hooks/useLoginSuccessHandler';
 import { usePageView } from '@packages/scoutgame-ui/hooks/usePageView';
+import { useRouter } from 'next/navigation';
+import { useAction } from 'next-safe-action/hooks';
 import { useEffect } from 'react';
 
 import WelcomeModal from './WelcomeModal';
@@ -12,7 +17,21 @@ import WelcomeModal from './WelcomeModal';
 // instantiate global hooks for the client-side only
 export function ClientGlobals({ userId }: { userId?: string }) {
   const platform = getPlatform();
+  const router = useRouter();
+  const { getNextPageLink } = useLoginSuccessHandler();
   const service = platform === 'telegram' ? 'scoutgametelegram-browser' : 'scoutgame-browser';
+  const { executeAsync: loginUser } = useAction(loginWithWalletAction, {
+    onSuccess: async ({ data }) => {
+      if (!data?.success) {
+        return;
+      }
+
+      router.push(getNextPageLink({ onboarded: data?.onboarded }));
+    },
+    onError(err) {
+      log.error('Error on login', { error: err.error.serverError });
+    }
+  });
 
   useDatadogLogger({ service, userId });
   usePageView();
@@ -23,12 +42,19 @@ export function ClientGlobals({ userId }: { userId?: string }) {
       const context = await sdk.context;
       // If context is not present we are not inside a farcaster client
       if (context) {
-        await sdk.actions.ready({});
         const nonce = Math.random().toString(36).substring(2, 10);
-        await sdk.actions.signIn({
+        const { signature, message } = await sdk.actions.signIn({
           nonce,
-          expirationTime: new Date(Date.now() + 5 * 60 * 1000).toISOString()
+          // 1 hour expiration time
+          expirationTime: new Date(Date.now() + 60 * 60 * 1000).toISOString()
         });
+        // Auto login user if they have a wallet connected
+        await loginUser({
+          signature,
+          message
+        });
+
+        await sdk.actions.ready({});
       }
     };
 
