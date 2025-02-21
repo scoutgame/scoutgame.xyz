ae1dd54929ade6d82315ecffb646028a1335c1d8
mattcasey <mattwad@gmail.com>
2025-02-21 12:26:49 -0700

    .

--- packages/scoutgame-ui/src/components/info/partner-rewards/GoodDollarPage.tsx
+++ packages/scoutgame-ui/src/components/info/partner-rewards/GoodDollarPage.tsx
@@ -1,4 +1,4 @@
-import { Alert, Link, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 
 import { InfoCard } from '../../common/DocumentPageContainer/components/InfoCard';
 import { List, ListItem } from '../../common/List';
@@ -8,7 +8,7 @@ export function GoodDollarPage() {
   return (
     <InfoPageContainer
       data-test='partner-page-good-dollar'
-      image='/images/info/rewards-partner-good-dollar.jpg'
+      image='/images/info/rewards-partner-good-dollar.png'
       title='Good Dollar'
     >
       <Document />
@@ -16,47 +16,11 @@ export function GoodDollarPage() {
   );
 }
 
-const tiers = [
-  {
-    name: 'Common',
-    reward: '50 cUSD'
-  },
-  {
-    name: 'Rare',
-    reward: '150 cUSD'
-  },
-  {
-    name: 'Epic',
-    reward: '250 cUSD'
-  },
-  {
-    name: 'Mythic',
-    reward: '350 cUSD'
-  },
-  {
-    name: 'Legendary',
-    reward: '450 cUSD'
-  }
-];
-
 const qualifiedProjects = [
-  'https://github.com/celo-org/faucet',
-  'https://github.com/celo-org/celo-composer',
-  'https://github.com/celo-org/composer-kit/issues',
-  'https://github.com/celo-org/svelte-template',
-  'https://github.com/celo-org/celo-composer-react-native',
-  'https://github.com/celo-org/celo-composer-flutter',
-  'https://github.com/celo-org/celo-farcaster-frames'
-];
-const previousProjects = [
-  'https://github.com/valora-inc/hooks',
   'https://github.com/GoodDollar/GoodWeb3-Mono',
+  'https://github.com/GoodDollar/GoodSdks',
   'https://github.com/GoodDollar/GoodCollective',
-  'https://github.com/Glo-Foundation/glo-wallet',
-  'https://github.com/Ubeswap/ubeswap-interface-v3',
-  'https://github.com/capsule-org/examples-hub/issues',
-  'https://github.com/distroinfinity/superflow',
-  'https://github.com/getpara/awesome-para'
+  'https://github.com/GoodDollar/GoodProtocolUI'
 ];
 
 function Document() {
@@ -74,100 +38,15 @@ function Document() {
         rewards. Those who qualify will be notified via email and must sign up for the Quadratic Funding (QF) round to
         receive their rewards. Donations and matching funds will be distributed at the end of the round.
       </Typography>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>GitHub Issue Tier</TableCell>
-            <TableCell align='right'>Reward</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tiers.map((tier) => (
-            <TableRow key={tier.name}>
-              <TableCell>{tier.name}</TableCell>
-              <TableCell align='right'>{tier.reward}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
       <Typography variant='h6' color='secondary' mt={2}>
-        How to contribute
+        Eligible GoodDollar Repositories
       </Typography>
-      <Typography>
-        Open Source contributions can be overwhelming when you are staring out. It is important to check a repository
-        for a contribution guide. If they don't offer one, you can refer back to a general one or use the{' '}
-        <Link href='https://docs.celo.org/general/ecosystem/contributors' target='_blank' rel='noreferrer'>
-          Open Source Contribution guide
-        </Link>{' '}
-        provided by Celo
-      </Typography>
-      <Alert severity='info'>
-        This is the chance for you to <strong>learn</strong> and <strong>grow</strong> and make connections. Make sure
-        to <strong>reply</strong> to repo-maintainers, implement their feedback and communicate!!! If anything is
-        unclear, ask! It is normal that a new repository can be overwhelming. So, communicate, communicate, communicate
-        and learn!
-      </Alert>
-      <Typography>
-        Here are some general <strong>steps to get started</strong> if you are new to open source contributions:
-      </Typography>
-      <List listStyleType='decimal'>
-        <ListItem>
-          Understand the repositories architecture and functions (e.g., if a repo is about EIP make sure you understand
-          the structure of EIPs). <strong>Read some code</strong> (best way to learn)
-        </ListItem>
-        <ListItem>Check the reported issue and make sure you understand what is asked of you</ListItem>
-        <ListItem>
-          Create a <strong>fork</strong> of the repository and clone your fork to your local machine. You need to fork
-          the repository, as you won't have direct write access to the main repository
-        </ListItem>
-        <ListItem>
-          <strong>Run the code</strong> and reproduce the same behaviour
-        </ListItem>
-        <ListItem>
-          <strong>Clarify</strong> all <strong>open questions</strong> from the issue. Make sure you dive deep into the
-          repo before asking questions but also don’t waste time getting lost. Most of the time, when formulating a
-          question, everything will become clear.
-        </ListItem>
-        <ListItem>If the coding language is unfamiliar to you, do a simple introduction course</ListItem>
-        <ListItem>
-          Optional: try test first approach, where you write the test first to make sure to check for the error that
-          occurs
-        </ListItem>
-        <ListItem>Implement your solution</ListItem>
-        <ListItem>
-          Write <strong>tests</strong> if you haven't done it yet
-        </ListItem>
-        <ListItem>
-          Write <strong>documentation</strong> (based on how documentation is done in that repository)
-        </ListItem>
-        <ListItem>
-          Create a PR mentioning the Issue and explaining the solution, this will help the repo maintainer understand
-          what you did and merge your PR quickly
-        </ListItem>
-        <ListItem>
-          Implement any <strong>feedback</strong> the maintainer asks for
-        </ListItem>
-      </List>
-      <Typography variant='h6' color='secondary' mt={2}>
-        Qualified Celo Projects:
-      </Typography>
+      <Typography>Developers can contribute to the following repositories:</Typography>
       <List>
-        {qualifiedProjects.map((project) => (
-          <ListItem key={project}>
-            <Link href={project} target='_blank' rel='noreferrer'>
-              {project}
-            </Link>
-          </ListItem>
-        ))}
-      </List>
-      <Typography variant='h6' color='secondary' mt={2}>
-        No new bounties (for now):
-      </Typography>
-      <List>
-        {previousProjects.map((project) => (
-          <ListItem key={project}>
-            <Link href={project} target='_blank' rel='noreferrer'>
-              {project}
+        {qualifiedProjects.map((repo) => (
+          <ListItem key={repo}>
+            <Link href={repo} target='_blank' rel='noreferrer'>
+              {repo}
             </Link>
           </ListItem>
         ))}
