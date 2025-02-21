01d4cb521190c5b27d722b781cfde409445dc0fc
Matt Casey <mattwad@gmail.com>
2025-02-21 05:01:51 -0700

    better layout for builder table on mobile (#330)

--- packages/scoutgame-ui/src/components/scout/ScoutPageTable/components/CommonTableRow.tsx
+++ packages/scoutgame-ui/src/components/scout/ScoutPageTable/components/CommonTableRow.tsx
@@ -28,3 +28,25 @@ export const tableRowSx = {
 export function CommonTableRow({ children, sx }: { children: React.ReactNode; sx?: SxProps }) {
   return <TableRow sx={{ ...tableRowSx, ...sx }}>{children}</TableRow>;
 }
+
+export const tableRowNoPaddingSx = {
+  boxShadow: '2px 2px 2px 0px rgba(0, 0, 0, 0.25)',
+  [`& .${tableCellClasses.root}`]: {
+    paddingLeft: '6px',
+    paddingRight: '6px',
+    borderBottom: '1px solid',
+    borderBottomColor: 'background.default',
+    '&:first-of-type': {
+      paddingLeft: {
+        xs: '10px',
+        md: '20px'
+      }
+    },
+    '&:last-child': {
+      paddingRight: {
+        xs: '10px',
+        md: '20px'
+      }
+    }
+  }
+};
