f02bd6e4d164a565c1b7ceee25fb4b57e8b644d5
mattcasey <mattwad@gmail.com>
2025-02-24 11:40:58 -0700

    better week selector

--- apps/admin/components/partners/PartnersDashboard.tsx
+++ apps/admin/components/partners/PartnersDashboard.tsx
@@ -16,13 +16,21 @@ import {
   Typography,
   FormControl
 } from '@mui/material';
-import { getLastWeek, getWeekStartEndFormatted, getDateFromISOWeek } from '@packages/dates/utils';
+import {
+  getLastWeek,
+  getWeekStartEndFormatted,
+  getDateFromISOWeek,
+  getAllISOWeeksFromSeasonStart,
+  getCurrentWeek
+} from '@packages/dates/utils';
 import type { BonusPartner } from '@packages/scoutgame/bonus';
 import { bonusPartnersRecord } from '@packages/scoutgame/bonus';
 import Image from 'next/image';
 import React, { useState, useMemo } from 'react';
 
 import { FileDownloadButton } from 'components/common/FileDownloadButton';
+
+const allWeeks = getAllISOWeeksFromSeasonStart();
 
 export function PartnersDashboard() {
   return (
@@ -49,7 +57,9 @@ function PartnerCard({
   partnerName: string;
   children?: React.ReactNode;
 }) {
-  const lastWeek = getWeekStartEndFormatted(getDateFromISOWeek(getLastWeek()).toJSDate());
+  const currentWeek = getCurrentWeek();
+  const [selectedWeek, setSelectedWeek] = useState(currentWeek);
+
   return (
     <Card sx={{ p: 3, display: 'flex' }}>
       <Stack flex={1}>
@@ -62,18 +72,23 @@ function PartnerCard({
 
       <Stack direction='row' spacing={2}>
         <FormControl size='small'>
-          <Select value='current' sx={{ minWidth: 120 }}>
-            <MenuItem value='current'>Current Week</MenuItem>
-            <MenuItem value='last'>Last Week</MenuItem>
-            <MenuItem value='custom'>Custom Range</MenuItem>
+          <Select value={selectedWeek} onChange={(e) => setSelectedWeek(e.target.value)} sx={{ minWidth: 120 }}>
+            {allWeeks.map((week, index) => (
+              <MenuItem key={week} value={week}>
+                <Stack width='100%' gap={1} direction='row' justifyContent='space-between'>
+                  <Typography>{week === currentWeek ? 'Current Week' : `Week ${index + 1}`}</Typography>
+                  <Typography color='secondary'>{`${getDateFromISOWeek(week).toFormat('MMM d')}`}</Typography>
+                </Stack>
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
         <div>
           <FileDownloadButton
             variant='contained'
             size='small'
-            filename={`${partnerName} Weekly Report.tsv`}
-            src={`/api/partners/${partner}`}
+            filename={`${partnerName} Export - Week ${allWeeks.indexOf(selectedWeek) + 1} - ${getWeekStartEndFormatted(getDateFromISOWeek(selectedWeek).toJSDate())}.tsv`}
+            src={`/api/partners/${partner}?week=${selectedWeek}`}
           >
             Export
           </FileDownloadButton>
