87cbbcfd145b89b696cfff428f76568cae70a921
Safwan Shaheer <devorein00@gmail.com>
2025-02-25 10:06:57 +0600

    added logs

--- packages/scoutgame-ui/src/hooks/useInitFarcasterData.ts
+++ packages/scoutgame-ui/src/hooks/useInitFarcasterData.ts
@@ -14,13 +14,22 @@ export function useInitFarcasterData() {
   const { executeAsync: loginUser } = useAction(loginWithWalletAction, {
     onSuccess: async ({ data }) => {
       if (!data?.success) {
+        log.error('Error on farcaster frames login', { error: new Error('Farcaster frames login failed') });
         return;
       }
 
       router.push(getNextPageLink({ onboarded: data?.onboarded }));
+      // Wait for the router to push the page
+      await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
     },
     onError(err) {
-      log.error('Error on login', { error: err.error.serverError });
+      log.error('Error on farcaster frames login', { error: err.error.serverError });
+    },
+    onSettled: async () => {
+      // Signal that the frame is ready and hide the splash screen, regardless of whether the login was successful or not
+      await sdk.actions.ready({});
     }
   });
 
@@ -29,9 +38,8 @@ export function useInitFarcasterData() {
       const context = await sdk.context;
       // If context is not present we are not inside a farcaster client
       if (context) {
-        const nonce = Math.random().toString(36).substring(2, 10);
         const { signature, message } = await sdk.actions.signIn({
-          nonce,
+          nonce: Math.random().toString(36).substring(2, 10),
           // 1 hour expiration time
           expirationTime: new Date(Date.now() + 60 * 60 * 1000).toISOString()
         });
@@ -40,16 +48,11 @@ export function useInitFarcasterData() {
           signature,
           message
         });
-
-        await sdk.actions.ready({});
       }
     };
 
     if (sdk) {
       load();
-      return () => {
-        sdk.removeAllListeners();
-      };
     }
   }, []);
 }
