b54c7fcd94732198d0c901420648c486f15bd0ce
mattcasey <mattwad@gmail.com>
2025-03-11 13:02:53 -0600

    fix cache

--- apps/scoutgame/app/(general)/info/repositories/page.tsx
+++ apps/scoutgame/app/(general)/info/repositories/page.tsx
@@ -1,5 +1,3 @@
-'server-only';
-
 import { getPopularRepos } from '@packages/scoutgame/repos/getRepos';
 import { RepositoriesPage } from '@packages/scoutgame-ui/components/info/pages/RepositoriesPage';
 import type { Metadata } from 'next';
@@ -7,6 +5,8 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Repositories'
 };
+
+export const dynamic = 'force-dynamic';
 
 export default async function Repositories() {
   const popularRepos = await getPopularRepos();
