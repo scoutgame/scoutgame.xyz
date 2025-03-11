b50c12dacaaa424156d8e159002eebc49c287c99
Matt Casey <mattwad@gmail.com>
2025-03-11 13:13:28 -0600

    Fix cache issue in building app for CI + fix build error (#388)
    * mark repository page server-only to fix build error
    
    * fix cache

--- apps/scoutgame/app/(general)/info/repositories/page.tsx
+++ apps/scoutgame/app/(general)/info/repositories/page.tsx
@@ -6,6 +6,8 @@ export const metadata: Metadata = {
   title: 'Repositories'
 };
 
+export const dynamic = 'force-dynamic';
+
 export default async function Repositories() {
   const popularRepos = await getPopularRepos();
   return <RepositoriesPage popularRepos={popularRepos} />;
