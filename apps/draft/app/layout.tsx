19d4d1f872fc39b20373c534cee3ba2b5f7ffad4
mattcasey <mattwad@gmail.com>
2025-03-26 15:37:11 -0600

    add new draft app

---
+++ apps/draft/app/layout.tsx
@@ -0,0 +1,51 @@
+import { AppProviders } from '@packages/scoutgame-ui/providers/AppProviders';
+import type { Metadata, Viewport } from 'next';
+import type { ReactNode } from 'react';
+
+import '@/theme/styles.scss';
+
+const appName = 'Scout Game Draft';
+
+export const metadata: Metadata = {
+  applicationName: appName,
+  icons: {
+    icon: ['/favicon.ico'],
+    apple: ['/favicon.ico']
+  },
+  title: appName,
+  formatDetection: {
+    telephone: false
+  },
+  openGraph: {
+    type: 'website',
+    siteName: appName,
+    images: 'https://scoutgame.xyz/images/manifest/scoutgame-logo-256.png',
+    title: appName,
+    description: 'Scout. Build. Win.'
+  },
+  twitter: {
+    card: 'summary',
+    title: appName,
+    description: 'Scout. Build. Win.'
+  }
+};
+export const viewport: Viewport = {
+  themeColor: '#000',
+  userScalable: false
+};
+export default async function RootLayout({
+  children
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang='en' dir='ltr'>
+      <body>
+        {/* load env vars for the frontend - note that the parent body tag is required for React to not complain */}
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src='/__ENV.js' />
+        <AppProviders>{children}</AppProviders>
+      </body>
+    </html>
+  );
+}
