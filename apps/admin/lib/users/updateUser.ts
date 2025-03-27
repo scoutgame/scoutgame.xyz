94d9d32c11efa41d2090308bbd84face1ccdbee8
Matt Casey <mattwad@gmail.com>
2025-03-27 11:45:39 -0600

    enable devs reapplying (#436)
    * enable devs reapplying
    
    * fix types and show app date
    
    * fix types

--- apps/admin/lib/users/updateUser.ts
+++ apps/admin/lib/users/updateUser.ts
@@ -1,6 +1,17 @@
+import { log } from '@charmverse/core/log';
 import type { BuilderStatus } from '@charmverse/core/prisma';
 import { prisma } from '@charmverse/core/prisma-client';
 
 export async function setBuilderStatus(userId: string, status: BuilderStatus) {
-  return prisma.scout.update({ where: { id: userId }, data: { builderStatus: status } });
+  const scout = await prisma.scout.update({ where: { id: userId }, data: { builderStatus: status } });
+  if (status === 'applied') {
+    await prisma.scout.update({
+      where: { id: userId },
+      data: { reappliedAt: new Date() }
+    });
+    log.info(`Developer reapplied`, {
+      userId
+    });
+  }
+  return scout;
 }
