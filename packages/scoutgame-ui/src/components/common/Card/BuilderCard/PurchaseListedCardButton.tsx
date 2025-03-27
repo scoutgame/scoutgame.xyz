caed2c202e26a88b7ca5b0e5d3ab0003c7915abd
Safwan Shaheer <devorein00@gmail.com>
2025-03-27 14:35:57 +0600

    open wallet connect dialog when clicking on listing purchase button

--- packages/scoutgame-ui/src/components/common/Card/BuilderCard/PurchaseListedCardButton.tsx
+++ packages/scoutgame-ui/src/components/common/Card/BuilderCard/PurchaseListedCardButton.tsx
@@ -2,9 +2,41 @@ import { Button } from '@mui/material';
 import { builderTokenDecimals } from '@packages/scoutgame/builderNfts/constants';
 import { devTokenDecimals } from '@packages/scoutgame/protocol/constants';
 import { isOnchainPlatform } from '@packages/utils/platform';
+import { RainbowKitProvider, useConnectModal } from '@rainbow-me/rainbowkit';
 import Image from 'next/image';
+import { useAccount } from 'wagmi';
 
-import { scoutProtocolChain } from '../../../../../../scoutgame/src/protocol/constants';
+function PurchaseListedCardButtonComponent({
+  listing
+}: {
+  listing: {
+    id: string;
+    price: string;
+  };
+}) {
+  const { openConnectModal, connectModalOpen } = useConnectModal();
+  const { address } = useAccount();
+  const isOnchain = isOnchainPlatform();
+  const listingPrice = isOnchain
+    ? Number(BigInt(listing.price) / BigInt(10 ** devTokenDecimals))
+    : Number(listing.price) / 10 ** builderTokenDecimals;
+
+  return (
+    <Button
+      variant='buy'
+      color='secondary'
+      fullWidth
+      onClick={() => {
+        if (!address && !connectModalOpen) {
+          openConnectModal?.();
+        }
+      }}
+    >
+      {listingPrice} &nbsp;{' '}
+      {isOnchain ? 'DEV' : <Image src='/images/crypto/usdc.png' alt='usdc' width={18} height={18} />}
+    </Button>
+  );
+}
 
 export function PurchaseListedCardButton({
   listing
@@ -14,15 +46,9 @@ export function PurchaseListedCardButton({
     price: string;
   };
 }) {
-  const isOnchain = isOnchainPlatform();
-  const listingPrice = isOnchain
-    ? Number(BigInt(listing.price) / BigInt(10 ** devTokenDecimals))
-    : Number(listing.price) / 10 ** builderTokenDecimals;
-
   return (
-    <Button variant='buy' color='secondary' fullWidth>
-      {listingPrice} &nbsp;{' '}
-      {isOnchain ? 'DEV' : <Image src='/images/crypto/usdc.png' alt='usdc' width={18} height={18} />}
-    </Button>
+    <RainbowKitProvider>
+      <PurchaseListedCardButtonComponent listing={listing} />
+    </RainbowKitProvider>
   );
 }
