9acc0b4a982b955c2488233cd381a0acbe654b92
mattcasey <mattwad@gmail.com>
2025-04-01 15:05:24 -0600

    fix types

--- packages/mailer/src/mailgunClient.ts
+++ packages/mailer/src/mailgunClient.ts
@@ -4,6 +4,7 @@ import Mailgun from 'mailgun.js';
 export const API_KEY = process.env.MAILGUN_API_KEY as string | undefined;
 export const DOMAIN = process.env.MAILGUN_DOMAIN as string;
 const mailgun = new Mailgun(formData);
-const client = API_KEY && API_KEY !== 'test-key' ? mailgun.client({ username: 'api', key: API_KEY }) : null;
+const client: ReturnType<typeof mailgun.client> | null =
+  API_KEY && API_KEY !== 'test-key' ? mailgun.client({ username: 'api', key: API_KEY }) : null;
 
 export default client;
