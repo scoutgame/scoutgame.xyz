f12cc59b1fbdd4c74335cc409e515ff3ff0ff846
Safwan Shaheer <devorein00@gmail.com>
2025-04-04 00:39:51 +0600

    refactoring

--- apps/airdrop/components/claim/components/DonationSelectionStep.tsx
+++ apps/airdrop/components/claim/components/DonationSelectionStep.tsx
@@ -12,6 +12,38 @@ type DonationOptionProps = {
   rightIcon?: string;
 };
 
+const StyledCard = styled(Card, {
+  shouldForwardProp: (prop) => prop !== 'selected'
+})<{ selected?: boolean }>(({ theme, selected }) => ({
+  width: 'fit-content',
+  padding: theme.breakpoints.down('md') ? theme.spacing(1.5) : theme.spacing(2),
+  borderRadius: theme.breakpoints.down('md') ? theme.spacing(1) : theme.spacing(2),
+  borderWidth: theme.breakpoints.down('md') ? theme.spacing(0.125) : theme.spacing(0.25),
+  borderColor: theme.palette.primary.main,
+  flex: 1,
+  flexDirection: 'column',
+  gap: theme.breakpoints.down('md') ? theme.spacing(0.5) : theme.spacing(1),
+  display: 'flex',
+  cursor: 'pointer',
+  backgroundColor: selected ? theme.palette.primary.main : theme.palette.background.paper,
+  height: 175,
+  justifyContent: 'center',
+  alignItems: 'center',
+  transition: theme.transitions.create(['background-color', 'border-color'], {
+    duration: 150,
+    easing: 'ease-in-out'
+  }),
+  '&:hover': selected
+    ? undefined
+    : {
+        transition: theme.transitions.create(['background-color', 'border-color'], {
+          duration: 150,
+          easing: 'ease-in-out'
+        }),
+        backgroundColor: theme.palette.background.light
+      }
+}));
+
 function DonationOption({ selected, onClick, title, description, leftIcon, rightIcon }: DonationOptionProps) {
   const isDesktop = useMdScreen();
 
