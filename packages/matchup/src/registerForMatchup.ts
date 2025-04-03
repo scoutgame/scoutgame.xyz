ea8a1660e79c8c91aa6e54ae770ffe38b59dbd03
mattcasey <mattwad@gmail.com>
2025-04-02 23:20:26 -0600

    .

--- packages/matchup/src/registerForMatchup.ts
+++ packages/matchup/src/registerForMatchup.ts
@@ -1,15 +1,15 @@
 import { prisma } from '@charmverse/core/prisma-client';
-import { getCurrentWeek, getNextWeek } from '@packages/dates/utils';
+import { getNextWeek, getWeekFromDate } from '@packages/dates/utils';
 import { sendPointsForMiscEvent } from '@packages/scoutgame/points/builderEvents/sendPointsForMiscEvent';
 import { DateTime } from 'luxon';
 
 import { REGISTRATION_DAY_OF_WEEK, MATCHUP_REGISTRATION_FEE } from './config';
 
 // scouts can only register for a matchup in the next week, unless it is Monday of the current week
-export function isValidRegistrationWeek(week: string) {
-  const currentWeek = getCurrentWeek();
+export function isValidRegistrationWeek(week: string, now = DateTime.utc()) {
+  const currentWeek = getWeekFromDate(now.toJSDate());
   const nextWeek = getNextWeek(currentWeek);
-  const currentDay = DateTime.utc().weekday;
+  const currentDay = now.weekday;
   return currentDay === REGISTRATION_DAY_OF_WEEK ? week === currentWeek : week === nextWeek;
 }
 
