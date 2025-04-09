0c89061ce978f283369b440423640bf13ecb4a3a
Matt Casey <mattwad@gmail.com>
2025-04-08 23:20:31 -0600

    responsive matchup leaderboard (#490)
    * responsive matchup leaderboard
    
    * responsive leaderboard
    
    * fix header

--- apps/scoutgame/components/matchup/components/RegistrationHeader.tsx
+++ apps/scoutgame/components/matchup/components/RegistrationHeader.tsx
@@ -33,13 +33,18 @@ export function RegistrationHeader({
         gap={{ xs: 2, md: 0 }}
       >
         <Stack direction={{ xs: 'column', md: 'row' }} alignItems='center' gap={2}>
-          <Box mr={{ xs: 0, md: 2 }}>
+          <Box mr={2} display={{ xs: 'none', md: 'block' }}>
             <Image src='/images/matchup/vs_icon.svg' alt='' width={80} height={80} />
           </Box>
           <Stack gap={1}>
-            <Typography variant='h4' color='secondary' fontWeight={400}>
-              Week {weekNumber} Match Up!
-            </Typography>
+            <Box alignItems='center' gap={1} display='flex'>
+              <Box display={{ xs: 'block', md: 'none' }}>
+                <Image src='/images/matchup/vs_icon.svg' alt='' width={80} height={80} />
+              </Box>
+              <Typography variant='h4' color='secondary' fontWeight={400}>
+                Week {weekNumber} Match Up!
+              </Typography>
+            </Box>
             <Box>
               {registrationOpen || registered ? (
                 <RegistrationButton registered={registered} week={week} />
@@ -63,24 +68,26 @@ export function RegistrationHeader({
             </Typography>
           </Stack>
         </Stack>
-        <Box display='flex' flexDirection={{ xs: 'row', md: 'column' }} alignItems='center' gap={2}>
-          <Typography
-            variant='h6'
-            color='secondary'
-            sx={{ fontSize: { xs: 14, md: 20 }, display: 'flex', alignItems: 'center', gap: 0.5 }}
-            noWrap
-          >
-            <Image src='/images/matchup/howtoplay_trophy.svg' alt='' width={20} height={20} /> Prize Pool
-          </Typography>
-          <Box>
-            <Typography component='span' sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }} noWrap>
-              {matchupPool ? (
-                <>
-                  {matchupPool} <PointsIcon /> +{' '}
-                </>
-              ) : null}
-              {opPrize} <Image width={14} height={14} src='/images/crypto/op.png' alt='' />
+        <Box textAlign='center'>
+          <Box display='flex' flexDirection={{ xs: 'row', md: 'column' }} alignItems='center' gap={2} mb={1}>
+            <Typography
+              variant='h6'
+              color='secondary'
+              sx={{ fontSize: { xs: 14, md: 20 }, display: 'flex', alignItems: 'center', gap: 0.5 }}
+              noWrap
+            >
+              <Image src='/images/matchup/howtoplay_trophy.svg' alt='' width={20} height={20} /> Prize Pool
             </Typography>
+            <Box>
+              <Typography component='span' sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }} noWrap>
+                {matchupPool ? (
+                  <>
+                    {matchupPool} <PointsIcon /> +{' '}
+                  </>
+                ) : null}
+                {opPrize} <Image width={14} height={14} src='/images/crypto/op.png' alt='' />
+              </Typography>
+            </Box>
           </Box>
           <Typography component='em' variant='body2' color='grey' noWrap>
             80% of registration fees
