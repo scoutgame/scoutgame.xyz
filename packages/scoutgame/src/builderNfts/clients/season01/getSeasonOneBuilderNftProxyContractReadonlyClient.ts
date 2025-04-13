dceca9e17a9485cc555087cd85a7fd20973244dc
Safwan Shaheer <devorein00@gmail.com>
2025-04-14 02:05:04 +0600

    use 2025-w18 season

--- packages/scoutgame/src/builderNfts/clients/season01/getSeasonOneBuilderNftProxyContractReadonlyClient.ts
+++ packages/scoutgame/src/builderNfts/clients/season01/getSeasonOneBuilderNftProxyContractReadonlyClient.ts
@@ -7,7 +7,7 @@ import { ScoutGameSeason01NFTUpgradeableClient } from './wrappers/ScoutGameSeaso
 
 export function getSeasonOneBuilderNftProxyContractReadonlyClient() {
   const chain = base;
-  const contractAddress = getBuilderNftContractAddress('2025-W17');
+  const contractAddress = getBuilderNftContractAddress('2025-W18');
   return new ScoutGameSeason01NFTUpgradeableClient({
     chain,
     contractAddress,
