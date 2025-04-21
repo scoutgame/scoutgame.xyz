0782e1ff0561d196513902f245ce063361c4c58d
Safwan Shaheer <devorein00@gmail.com>
2025-04-21 07:25:06 +0600

    use is draft live env var

--- apps/scoutgame/lib/airdrop/checkAirdropDates.ts
+++ apps/scoutgame/lib/airdrop/checkAirdropDates.ts
@@ -1,12 +1,6 @@
 import env from '@beam-australia/react-env';
-import { DateTime } from 'luxon';
-
-export const AIRDROP_START_DATE = env('AIRDROP_START_DATE') || process.env.REACT_APP_AIRDROP_START_DATE;
 
 export function isAirdropLive() {
-  if (!AIRDROP_START_DATE) {
-    return false;
-  }
-  const now = DateTime.now().toUTC();
-  return now > DateTime.fromISO(AIRDROP_START_DATE);
+  const isDraftLive = env('IS_DRAFT_LIVE') || process.env.REACT_APP_IS_DRAFT_LIVE;
+  return isDraftLive === 'true';
 }
--- packages/scoutgame/src/drafts/checkDraftDates.ts
+++ packages/scoutgame/src/drafts/checkDraftDates.ts
@@ -1,21 +1,24 @@
 import env from '@beam-australia/react-env';
+import { isDraftSeason } from '@packages/dates/utils';
 import { DateTime } from 'luxon';
 
 const DRAFT_END_DATE = DateTime.fromISO('2025-04-25T23:59:59.999Z', { zone: 'utc' });
-// 11:00 EST, 15:00 UTC
-const DRAFT_START_DATE = env('AIRDROP_START_DATE') || process.env.REACT_APP_AIRDROP_START_DATE;
 
-export function isDraftEnded(): boolean {
+export function hasDraftEnded(): boolean {
   const nowUtc = DateTime.utc();
   return nowUtc > DRAFT_END_DATE;
 }
 
 export function isDraftEnabled(): boolean {
-  if (!DRAFT_START_DATE) {
+  const draftSeason = isDraftSeason();
+  if (!draftSeason) {
     return false;
   }
-  const nowUtc = DateTime.utc();
-  const draftStartDate = DateTime.fromISO(DRAFT_START_DATE);
+  const nowUtc = DateTime.now().toUTC();
+  if (nowUtc > DRAFT_END_DATE) {
+    return false;
+  }
 
-  return nowUtc > draftStartDate && nowUtc < DRAFT_END_DATE;
+  const isDraftLive = env('IS_DRAFT_LIVE') || process.env.REACT_APP_IS_DRAFT_LIVE;
+  return isDraftLive === 'true';
 }
