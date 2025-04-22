08264b49690575da492022b7d0979cc85cdda6b5
mattcasey <mattwad@gmail.com>
2025-04-22 11:28:15 -0600

    fix types

--- packages/scoutgameattestations/src/queries/graphql.ts
+++ packages/scoutgameattestations/src/queries/graphql.ts
@@ -1,7 +1,7 @@
 // Import direct from index.js necessary to avoid a bug with jest
 // https://github.com/apollographql/apollo-feature-requests/issues/287#issuecomment-1192993207
 import { ApolloClient, InMemoryCache } from '@apollo/client/core/index.js';
-import { base, baseSepolia, optimism, optimismSepolia } from 'viem/chains';
+import { base, optimism, optimismSepolia } from 'viem/chains';
 
 import type { EASSchemaChain } from '../easSchemas/constants';
 
@@ -10,12 +10,10 @@ import type { EASSchemaChain } from '../easSchemas/constants';
 export const optimismEasGraphqlUri = 'https://optimism.easscan.org/graphql';
 export const optimismSepoliaEasGraphqlUri = 'https://optimism-sepolia.easscan.org/graphql';
 export const baseEasGraphqlUri = 'https://base.easscan.org/graphql';
-export const baseSepoliaEasGraphqlUri = 'https://base-sepolia.easscan.org/graphql';
 
 export function getEasGraphQlClient({ chainId }: { chainId: EASSchemaChain }): ApolloClient<any> {
   const uriMap: Record<EASSchemaChain, string> = {
     [optimism.id]: optimismEasGraphqlUri,
-    [baseSepolia.id]: baseSepoliaEasGraphqlUri,
     [optimismSepolia.id]: optimismSepoliaEasGraphqlUri
   };
 
