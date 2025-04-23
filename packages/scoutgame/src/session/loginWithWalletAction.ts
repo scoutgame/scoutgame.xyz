55b2277b65c9847dd71b4a660e9b42c29af2a0b6
mattcasey <mattwad@gmail.com>
2025-04-23 14:11:40 -0600

    fixes

--- packages/scoutgame/src/session/loginWithWalletAction.ts
+++ packages/scoutgame/src/session/loginWithWalletAction.ts
@@ -15,50 +15,51 @@ import { connectWalletAccountSchema } from '../wallets/connectWalletAccountSchem
 import { verifyWalletSignature } from '../wallets/verifyWalletSignature';
 
 export const loginWithWalletAction = actionClient
-  .metadata({ actionName: 'login_with_wallet' })
-  .schema(connectWalletAccountSchema)
+  // .schema(connectWalletAccountSchema)
   .action(async ({ ctx, parsedInput }) => {
-    const newUserId = ctx.session.anonymousUserId;
-    const { walletAddress } = await verifyWalletSignature(parsedInput);
+    // eslint-disable-next-line no-console
+    console.log('loginWithWalletAction', ctx, parsedInput);
+    // const newUserId = ctx.session.anonymousUserId;
+    // const { walletAddress } = await verifyWalletSignature(parsedInput);
 
-    if (parsedInput.inviteCode) {
-      const cookieStore = await cookies();
-      cookieStore.set(
-        'invite-code',
-        await sealData({ inviteCode: parsedInput.inviteCode }, { password: authSecret as string })
-      );
-      log.info(`Builder logged in with invite code: ${parsedInput.inviteCode}`, { userId: newUserId });
-    }
+    // if (parsedInput.inviteCode) {
+    //   const cookieStore = await cookies();
+    //   cookieStore.set(
+    //     'invite-code',
+    //     await sealData({ inviteCode: parsedInput.inviteCode }, { password: authSecret as string })
+    //   );
+    //   log.info(`Builder logged in with invite code: ${parsedInput.inviteCode}`, { userId: newUserId });
+    // }
 
-    const user = await findOrCreateWalletUser({
-      wallet: walletAddress,
-      newUserId,
-      referralCode: parsedInput.referralCode,
-      utmCampaign: parsedInput.utmCampaign
-    });
-    await saveSession(ctx, { scoutId: user.id });
-    const sessionUser = (await getUserFromSession()) as SessionUser;
+    // const user = await findOrCreateWalletUser({
+    //   wallet: walletAddress,
+    //   newUserId,
+    //   referralCode: parsedInput.referralCode,
+    //   utmCampaign: parsedInput.utmCampaign
+    // });
+    // await saveSession(ctx, { scoutId: user.id });
+    // const sessionUser = (await getUserFromSession()) as SessionUser;
 
-    log.info('User logged in', {
-      userId: user.id,
-      newUserId,
-      referralCode: parsedInput.referralCode || undefined,
-      utmCampaign: parsedInput.utmCampaign || undefined
-    });
+    // log.info('User logged in', {
+    //   userId: user.id,
+    //   newUserId,
+    //   referralCode: parsedInput.referralCode || undefined,
+    //   utmCampaign: parsedInput.utmCampaign || undefined
+    // });
 
-    if (user.isNew) {
-      trackUserAction('sign_up', {
-        userId: user.id
-      });
-    } else {
-      trackUserAction('sign_in', {
-        userId: user.id
-      });
-    }
+    // if (user.isNew) {
+    //   trackUserAction('sign_up', {
+    //     userId: user.id
+    //   });
+    // } else {
+    //   trackUserAction('sign_in', {
+    //     userId: user.id
+    //   });
+    // }
 
-    return {
-      user: sessionUser,
-      success: true,
-      onboarded: !!sessionUser.onboardedAt
-    };
+    // return {
+    //   user: sessionUser,
+    //   success: true,
+    //   onboarded: !!sessionUser.onboardedAt
+    // };
   });
