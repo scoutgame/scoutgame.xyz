f1af5375f88a9a4641b33cc62471bd590eea5b35
mattcasey <mattwad@gmail.com>
2025-04-23 15:40:28 -0600

    reset

--- packages/nextjs/src/session/logoutAction.ts
+++ packages/nextjs/src/session/logoutAction.ts
@@ -4,7 +4,7 @@ import { log } from '@charmverse/core/log';
 
 import { actionClient } from '../actions/actionClient';
 
-export const logoutAction = actionClient.action(async ({ ctx }) => {
+export const logoutAction = actionClient.metadata({ actionName: 'logout' }).action(async ({ ctx }) => {
   const userId = ctx.session.adminId || ctx.session.scoutId;
   ctx.session.destroy();
   await ctx.session.save();
