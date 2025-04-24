b31a1d86641f7c416d5554357d39a0bd675286aa
mattcasey <mattwad@gmail.com>
2025-04-23 22:31:40 -0600

    fix proxy

--- packages/scoutgame/src/protocol/clients/getProxyClient.ts
+++ packages/scoutgame/src/protocol/clients/getProxyClient.ts
@@ -1,3 +1,6 @@
+import { getPublicClient } from '@packages/blockchain/getPublicClient';
+import type { Address } from 'viem';
+
 import { scoutProtocolChainId } from '../constants';
 import { ProxyContract } from '../contracts/ProxyContract';
 
