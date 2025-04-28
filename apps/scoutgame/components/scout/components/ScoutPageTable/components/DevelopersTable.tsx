4637973f1f31a9f7e4e3d0e01c765b6059dd9b9c
mattcasey <mattwad@gmail.com>
2025-04-28 16:08:14 -0600

    show sold out on dev table

--- apps/scoutgame/components/scout/components/ScoutPageTable/components/DevelopersTable.tsx
+++ apps/scoutgame/components/scout/components/ScoutPageTable/components/DevelopersTable.tsx
@@ -3,6 +3,7 @@
 import NorthIcon from '@mui/icons-material/North';
 import SouthIcon from '@mui/icons-material/South';
 import { Stack, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import { maxDevTokenPrice } from '@packages/scoutgame/builderNfts/constants';
 import type { DeveloperMetadata, DevelopersSortBy } from '@packages/scoutgame/builders/getDevelopersForTable';
 import { devTokenDecimals } from '@packages/scoutgame/protocol/constants';
 import { Avatar } from '@packages/scoutgame-ui/components/common/Avatar';
@@ -135,111 +136,127 @@ export function DevelopersTable({
         </TableRow>
       </TableHead>
       <TableBody>
-        {developers.map((developer) => (
-          <TableRow
-            key={developer.path}
-            sx={tableRowNoPaddingSx}
-            onClick={() => openModal('developerInfo', { path: developer.path })}
-            style={{ cursor: 'pointer' }}
-          >
-            <TableCell>
-              <Stack
-                component={Link}
-                href={`/u/${developer.path}`}
-                onClick={(e) => {
-                  e.preventDefault();
+        {developers.map((developer) => {
+          const cardPrice = getPrice(developer.price);
+          return (
+            <TableRow
+              key={developer.path}
+              sx={tableRowNoPaddingSx}
+              onClick={() => openModal('developerInfo', { path: developer.path })}
+              style={{ cursor: 'pointer' }}
+            >
+              <TableCell>
+                <Stack
+                  component={Link}
+                  href={`/u/${developer.path}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                  }}
+                  alignItems='center'
+                  flexDirection='row'
+                  gap={{ xs: 0.75, md: 1.5 }}
+                  maxWidth={{ xs: '65px', md: 'initial' }}
+                >
+                  <Avatar src={developer.avatar} name={developer.displayName} size={isMdScreen ? 'medium' : 'xSmall'} />
+                  <Stack maxWidth={{ xs: '50px', md: 'initial' }}>
+                    {developer.nftsSoldToLoggedInScout ? (
+                      <Stack direction='row' alignItems='center' gap={0.5}>
+                        <Typography fontSize={{ xs: '10.5px', md: '14px' }} color='green.main' noWrap>
+                          {developer.nftsSoldToLoggedInScout}
+                        </Typography>
+                        <Image
+                          width={isMdScreen ? 15 : 13}
+                          height={isMdScreen ? 15 : 13}
+                          src='/images/profile/icons/cards-green.svg'
+                          alt='green-icon'
+                        />
+                      </Stack>
+                    ) : null}
+                    <TableCellText deskTopfontSize='16px' overflow='hidden' textOverflow='ellipsis' noWrap>
+                      {developer.displayName}
+                    </TableCellText>
+                  </Stack>
+                </Stack>
+              </TableCell>
+              <TableCell align='center' sx={{ display: 'table-cell' }}>
+                <TableCellText color='orange.main'>{developer.level}</TableCellText>
+              </TableCell>
+              <TableCell
+                padding='none'
+                sx={{
+                  width: 150,
+                  height: { xs: 35, md: 50 }
                 }}
-                alignItems='center'
-                flexDirection='row'
-                gap={{ xs: 0.75, md: 1.5 }}
-                maxWidth={{ xs: '65px', md: 'initial' }}
               >
-                <Avatar src={developer.avatar} name={developer.displayName} size={isMdScreen ? 'medium' : 'xSmall'} />
-                <Stack maxWidth={{ xs: '50px', md: 'initial' }}>
-                  {developer.nftsSoldToLoggedInScout ? (
-                    <Stack direction='row' alignItems='center' gap={0.5}>
-                      <Typography fontSize={{ xs: '10.5px', md: '14px' }} color='green.main' noWrap>
-                        {developer.nftsSoldToLoggedInScout}
-                      </Typography>
-                      <Image
-                        width={isMdScreen ? 15 : 13}
-                        height={isMdScreen ? 15 : 13}
-                        src='/images/profile/icons/cards-green.svg'
-                        alt='green-icon'
-                      />
-                    </Stack>
-                  ) : null}
-                  <TableCellText deskTopfontSize='16px' overflow='hidden' textOverflow='ellipsis' noWrap>
-                    {developer.displayName}
+                <Stack
+                  direction='row'
+                  alignItems='center'
+                  gap={{
+                    xs: 0.5,
+                    md: 1
+                  }}
+                  height='100%'
+                >
+                  <BuilderCardRankGraph ranks={developer.last14DaysRank} />
+                  <TableCellText
+                    color='secondary'
+                    minWidth={{
+                      xs: 20,
+                      md: 25
+                    }}
+                  >
+                    {developer.rank || '-'}
                   </TableCellText>
                 </Stack>
-              </Stack>
-            </TableCell>
-            <TableCell align='center' sx={{ display: 'table-cell' }}>
-              <TableCellText color='orange.main'>{developer.level}</TableCellText>
-            </TableCell>
-            <TableCell
-              padding='none'
-              sx={{
-                width: 150,
-                height: { xs: 35, md: 50 }
-              }}
-            >
-              <Stack
-                direction='row'
-                alignItems='center'
-                gap={{
-                  xs: 0.5,
-                  md: 1
-                }}
-                height='100%'
-              >
-                <BuilderCardRankGraph ranks={developer.last14DaysRank} />
-                <TableCellText
-                  color='secondary'
-                  minWidth={{
-                    xs: 20,
-                    md: 25
-                  }}
+              </TableCell>
+              <TableCell align='center'>
+                <Stack
+                  alignItems='center'
+                  flexDirection='row'
+                  gap={{ xs: 0.5, md: 1 }}
+                  ml={1.5}
+                  justifyContent='flex-end'
                 >
-                  {developer.rank || '-'}
-                </TableCellText>
-              </Stack>
-            </TableCell>
-            <TableCell align='center'>
-              <Stack
-                alignItems='center'
-                flexDirection='row'
-                gap={{ xs: 0.5, md: 1 }}
-                ml={1.5}
-                justifyContent='flex-end'
-              >
-                <TableCellText deskTopfontSize='16px'>{developer.gemsCollected}</TableCellText>
-                <Image
-                  width={isMdScreen ? 15 : 12.5}
-                  height={isMdScreen ? 15 : 12.5}
-                  src='/images/icons/gem.svg'
-                  alt='gem icon'
-                />
-              </Stack>
-            </TableCell>
-            <TableCell align='right' width={isMdScreen ? 150 : 'auto'}>
-              <Stack alignItems='center' flexDirection='row' gap={{ xs: 0.5, md: 1 }} justifyContent='flex-end'>
-                <TableCellText color='text.primary'>{developer.estimatedPayout}</TableCellText>
-                {isMdScreen && <Image width={15} height={15} src='/images/dev-token-logo.png' alt='DEV Token' />}
-              </Stack>
-            </TableCell>
-            <TableCell align='center'>
-              <Stack alignItems='center' flexDirection='row' gap={{ xs: 0.5, md: 1 }} justifyContent='flex-end'>
-                <TableCellText color='text.primary'>
-                  {Number(developer.price || 0) / 10 ** devTokenDecimals}
-                </TableCellText>
-                {isMdScreen && <Image width={15} height={15} src='/images/dev-token-logo.png' alt='DEV Token' />}
-              </Stack>
-            </TableCell>
-          </TableRow>
-        ))}
+                  <TableCellText deskTopfontSize='16px'>{developer.gemsCollected}</TableCellText>
+                  <Image
+                    width={isMdScreen ? 15 : 12.5}
+                    height={isMdScreen ? 15 : 12.5}
+                    src='/images/icons/gem.svg'
+                    alt='gem icon'
+                  />
+                </Stack>
+              </TableCell>
+              <TableCell align='right' width={isMdScreen ? 150 : 'auto'}>
+                <Stack alignItems='center' flexDirection='row' gap={{ xs: 0.5, md: 1 }} justifyContent='flex-end'>
+                  <TableCellText color='text.primary'>{developer.estimatedPayout}</TableCellText>
+                  {isMdScreen && <Image width={15} height={15} src='/images/dev-token-logo.png' alt='DEV Token' />}
+                </Stack>
+              </TableCell>
+              <TableCell align='center'>
+                <Stack alignItems='center' flexDirection='row' gap={{ xs: 0.5, md: 1 }} justifyContent='flex-end'>
+                  <TableCellText
+                    color={cardPrice === -1 ? 'grey' : 'text.primary'}
+                    sx={{ fontSize: cardPrice === -1 ? '12px' : '14px' }}
+                  >
+                    {cardPrice === -1 ? 'SOLD OUT' : cardPrice}
+                  </TableCellText>
+                  {isMdScreen && cardPrice !== -1 && (
+                    <Image width={15} height={15} src='/images/dev-token-logo.png' alt='DEV Token' />
+                  )}
+                </Stack>
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
+}
+
+function getPrice(price: bigint) {
+  const formattedPrice = Number(price || 0) / 10 ** devTokenDecimals;
+  if (formattedPrice === maxDevTokenPrice) {
+    return -1;
+  }
+  return formattedPrice;
 }
