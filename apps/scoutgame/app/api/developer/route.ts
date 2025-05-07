9a3bc4119ee7ca91689fd795a83cb0b54ee324dc
Safwan Shaheer <devorein00@gmail.com>
2025-05-07 20:29:20 +0600

    developer api route

---
+++ apps/scoutgame/app/api/developer/route.ts
@@ -0,0 +1,80 @@
+import type { Prisma } from '@charmverse/core/prisma-client';
+import { prisma } from '@charmverse/core/prisma-client';
+import { getCurrentSeasonStart } from '@packages/dates/utils';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const path = searchParams.get('path');
+  const wallet = searchParams.get('wallet');
+  const farcasterId = searchParams.get('farcasterId');
+  const farcasterName = searchParams.get('farcasterName');
+
+  if (!path && !wallet && !farcasterId && !farcasterName) {
+    return NextResponse.json({ error: 'No path, wallet, farcasterId, or farcasterName provided' }, { status: 400 });
+  }
+
+  const where: Prisma.ScoutWhereInput = {
+    builderStatus: 'approved'
+  };
+
+  if (path) {
+    where.path = path;
+  } else if (wallet) {
+    where.wallets = {
+      some: {
+        address: wallet.toLowerCase()
+      }
+    };
+  } else if (farcasterId) {
+    where.farcasterId = parseInt(farcasterId);
+  } else if (farcasterName) {
+    where.farcasterName = farcasterName;
+  }
+
+  const developer = await prisma.scout.findFirst({
+    where,
+    select: {
+      avatar: true,
+      displayName: true,
+      path: true,
+      userSeasonStats: {
+        where: {
+          season: getCurrentSeasonStart()
+        },
+        select: {
+          level: true
+        }
+      },
+      userWeeklyStats: {
+        where: {
+          season: getCurrentSeasonStart()
+        },
+        orderBy: {
+          week: 'asc'
+        },
+        select: {
+          week: true,
+          gemsCollected: true
+        }
+      }
+    }
+  });
+
+  if (!developer) {
+    return NextResponse.json({ developer: null }, { status: 200 });
+  }
+
+  return NextResponse.json({
+    developer: {
+      avatar: developer.avatar,
+      displayName: developer.displayName,
+      profileUrl: `https://scoutgame.xyz/u/${developer.path}`,
+      level: developer.userSeasonStats[0].level,
+      gemsCollected: developer.userWeeklyStats.map((weeklyStat) => ({
+        week: weeklyStat.week,
+        count: weeklyStat.gemsCollected
+      }))
+    }
+  });
+}
