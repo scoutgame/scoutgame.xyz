f0158df31661d232f1d6174e5fb061b4628c33ae
mattcasey <mattwad@gmail.com>
2025-05-07 22:53:20 -0600

    ensure that scouts still own the developer nft in matchup

--- packages/matchup/src/getMatchupLeaderboard.ts
+++ packages/matchup/src/getMatchupLeaderboard.ts
@@ -43,13 +43,24 @@ export async function getMatchupLeaderboard(week: string, limit?: number): Promi
           id: true,
           displayName: true,
           avatar: true,
-          path: true
+          path: true,
+          wallets: {
+            select: {
+              scoutedNfts: {
+                select: {
+                  id: true,
+                  balance: true
+                }
+              }
+            }
+          }
         }
       },
       selections: {
         select: {
           developerNft: {
             select: {
+              id: true,
               builder: {
                 select: {
                   id: true,
@@ -75,6 +86,10 @@ export async function getMatchupLeaderboard(week: string, limit?: number): Promi
   const leaderboard = entries
     .map((entry) => {
       const developers = entry.selections
+        // filter out selections if the scout no longer holds the NFT
+        .filter((selection) =>
+          entry.scout.wallets.some((w) => w.scoutedNfts.some((nft) => nft.id === selection.developerNft.id))
+        )
         .map((selection) => ({
           id: selection.developerNft!.builder.id,
           displayName: selection.developerNft!.builder.displayName,
