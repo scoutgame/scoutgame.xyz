8a027e9a0d4ec7d59cb26173217358c2dcb698aa
Safwan Shaheer <devorein00@gmail.com>
2025-06-06 01:12:54 +0600

    fixed link button action

--- apps/scoutgame/components/[path]/FarcasterMetadata.tsx
+++ apps/scoutgame/components/[path]/FarcasterMetadata.tsx
@@ -20,7 +20,7 @@ export function FarcasterMetadata({
       <meta property='fc:frame:image:aspect_ratio' content='1:1' />
       {/* Button 1 */}
       <meta name='fc:frame:button:1' content='Scout Developer' />
-      <meta name='fc:frame:button:1:action' content='post_redirect' />
+      <meta name='fc:frame:button:1:action' content='link' />
       <meta name='fc:frame:button:1:target' content={miniAppUrl} />
     </>
   );
