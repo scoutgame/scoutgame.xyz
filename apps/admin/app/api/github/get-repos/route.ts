7f8d3488235bd56b1c52eb4ef62643dc62f2bf61
Safwan Shaheer <devorein00@gmail.com>
2025-07-09 21:03:10 +0600

    fixed get repos using org and name

--- apps/admin/app/api/github/get-repos/route.ts
+++ apps/admin/app/api/github/get-repos/route.ts
@@ -1,3 +1,4 @@
+import type { Prisma } from '@charmverse/core/prisma-client';
 import { prisma } from '@charmverse/core/prisma-client';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
@@ -12,38 +13,40 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const partner = searchParams.get('partner') === 'true';
   const nameOrOwner = searchParams.get('nameOrOwner');
+  const [ownerOrName, name] = nameOrOwner?.replaceAll('https://github.com/', '').split('/') || [];
   if (!nameOrOwner) {
     return NextResponse.json([]);
   }
+
+  const where: Prisma.GithubRepoWhereInput = {
+    scoutPartnerId: partner ? { not: null } : undefined
+  };
+
+  if (ownerOrName && name) {
+    where.AND = [{ name }, { owner: ownerOrName }];
+  } else if (ownerOrName) {
+    where.OR = [{ owner: ownerOrName }, { name: ownerOrName }];
+  }
+
   const repos = await prisma.githubRepo.findMany({
-    where: {
-      scoutPartnerId: partner ? { not: null } : undefined,
-      OR: [
-        { name: { contains: nameOrOwner, mode: 'insensitive' } },
-        { owner: { contains: nameOrOwner, mode: 'insensitive' } }
-      ]
-    },
+    where,
     select: {
       id: true,
       owner: true,
       name: true
     },
-    take: 100,
-    orderBy: {
-      _relevance: {
-        fields: ['name', 'owner'],
-        search: nameOrOwner,
-        sort: 'desc'
-      }
-    }
+    take: 100
   });
 
-  const result: GetReposResult[] = repos.map((repo) => {
-    return {
-      id: repo.id,
-      fullName: `${repo.owner}/${repo.name}`,
-      url: `https://github.com/${repo.owner}/${repo.name}`
-    };
-  });
+  const result: GetReposResult[] = repos
+    .map((repo) => {
+      return {
+        id: repo.id,
+        fullName: `${repo.owner}/${repo.name}`,
+        url: `https://github.com/${repo.owner}/${repo.name}`
+      };
+    })
+    .sort((a, b) => a.fullName.localeCompare(b.fullName));
+
   return NextResponse.json(result);
 }
